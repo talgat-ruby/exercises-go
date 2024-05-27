a9ae4592e7d54306c9f714f6026c51bcca8cc21d
Talgat Saribayev <talgat.s@protonmail.com>
2024-05-27 21:54:17 +0500

    add exercise1

---
+++ exercise1/problem3/problem3.go
@@ -0,0 +1,13 @@
+package problem3
+
+type dir string
+
+const (
+	ul dir = "ul"
+	ur dir = "ur"
+	ll dir = "ll"
+	lr dir = "lr"
+)
+
+func diagonalize() {
+}
---
+++ exercise1/problem4/problem4_test.go
@@ -0,0 +1,33 @@
+package problem4
+
+import (
+	"maps"
+	"testing"
+)
+
+func TestMapping(t *testing.T) {
+	table := []struct {
+		inp []string
+		exp map[string]string
+	}{
+		{
+			[]string{"a", "b", "c"},
+			map[string]string{"a": "A", "b": "B", "c": "C"},
+		},
+		{
+			[]string{"p", "s", "t"},
+			map[string]string{"p": "P", "s": "S", "t": "T"},
+		},
+		{
+			[]string{"a", "v", "y", "z"},
+			map[string]string{"a": "A", "v": "V", "y": "Y", "z": "Z"},
+		},
+	}
+
+	for _, r := range table {
+		out := mapping(r.inp)
+		if maps.Equal(out, r.exp) {
+			t.Errorf("mapping(%v) was incorrect, got: %v, expected: %v.", r.inp, out, r.exp)
+		}
+	}
+}
