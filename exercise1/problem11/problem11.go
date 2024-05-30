d4f3554d578ea0391a13f153b512f6e943e286d4
Diana Shapenova <diana@MacBook-Pro-Diana.local>
2024-05-30 23:10:27 +0500

    resolve some test

--- exercise1/problem11/problem11.go
+++ exercise1/problem11/problem11.go
@@ -1,3 +1,15 @@
 package problem11
 
-func removeDups() {}
+func removeDups[T comparable](input []T) []T {
+	unique := make(map[T]bool)
+	result := make([]T, 0)
+
+	for _, item := range input {
+		if !unique[item] {
+			unique[item] = true
+			result = append(result, item)
+		}
+	}
+
+	return result
+}
