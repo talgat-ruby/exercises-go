bd0319d733fed5f03061739d50564e85b8dc5850
Artem Prima <a.prima@lerna.digital>
2024-05-30 18:51:05 +0500

    add problem1

--- exercise1/problem1/problem1.go
+++ exercise1/problem1/problem1.go
@@ -1,4 +1,6 @@
 package problem1
 
-func isChangeEnough() {
+func isChangeEnough(change [4]int, amount float32) bool {
+	total := float32(change[0])*0.25 + float32(change[1])*0.10 + float32(change[2])*0.05 + float32(change[3])*0.01
+	return total >= amount
 }
