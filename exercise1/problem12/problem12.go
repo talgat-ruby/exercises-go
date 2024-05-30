418dbdf8d67e11d82f88771f209aa6796c46832d
myrzakhmet <smagul.myrzakhmet@mail.ru>
2024-05-30 15:48:31 +0500

    solve problem 12

--- exercise1/problem12/problem12.go
+++ exercise1/problem12/problem12.go
@@ -1,3 +1,31 @@
 package problem11
 
-func keysAndValues() {}
+type Comparable interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 |
+		~string | ~float32 | ~float64
+}
+
+func keysAndValues[K Comparable, V any](m map[K]V) ([]K, []V) {
+	keys := make([]K, 0, len(m))
+	values := make([]V, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+
+	bubleSort(keys)
+	for _, v := range keys {
+		values = append(values, m[v])
+	}
+	return keys, values
+}
+
+func bubleSort[T Comparable](slice []T) {
+	for i := 0; i < len(slice)-1; i++ {
+		for j := 0; j < len(slice)-1-i; j++ {
+			if slice[j] > slice[j+1] {
+				slice[j], slice[j+1] = slice[j+1], slice[j]
+			}
+		}
+	}
+}
