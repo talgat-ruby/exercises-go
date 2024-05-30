9200ed50c557873f779b08df02985877e52f59c0
Artem Prima <a.prima@lerna.digital>
2024-05-30 18:51:11 +0500

    add problem2

--- exercise1/problem2/problem2.go
+++ exercise1/problem2/problem2.go
@@ -1,4 +1,20 @@
 package problem2
 
-func capitalize() {
+import (
+	"strings"
+	"unicode"
+)
+
+func capitalize(names []string) []string {
+	var capitalizedNames []string
+	for _, name := range names {
+		if len(name) > 0 {
+			first := string(unicode.ToUpper(rune(name[0])))
+			next := strings.ToLower(name[1:])
+			capitalizedNames = append(capitalizedNames, first+next)
+		} else {
+			capitalizedNames = append(capitalizedNames, "")
+		}
+	}
+	return capitalizedNames
 }
