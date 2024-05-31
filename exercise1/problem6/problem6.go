a88d0d347775ec47e6bec2425fb833aa13bfa087
Artem Prima <a.prima@lerna.digital>
2024-05-31 10:40:52 +0500

    add problem6

--- exercise1/problem6/problem6.go
+++ exercise1/problem6/problem6.go
@@ -1,4 +1,12 @@
 package problem6
 
-func sumOfTwo() {
+func sumOfTwo(a []int, b []int, v int) bool {
+	for _, numA := range a {
+		for _, numB := range b {
+			if numA+numB == v {
+				return true
+			}
+		}
+	}
+	return false
 }
