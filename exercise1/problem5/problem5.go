a17ae6365b02cce22ef3a50d919fee7cdfb07da5
asabiyev <asabiyev@jmart.kz>
2024-05-31 18:10:01 +0500

    adil sabiyev exercise 1

--- exercise1/problem5/problem5.go
+++ exercise1/problem5/problem5.go
@@ -1,4 +1,32 @@
 package problem5
 
-func products() {
+import "sort"
+
+type Product struct {
+	Name  string
+	Price int
+}
+
+func products(catalog map[string]int, minPrice int) []string {
+	var products []Product
+
+	for name, price := range catalog {
+		if price >= minPrice {
+			products = append(products, Product{name, price})
+		}
+	}
+
+	sort.Slice(products, func(i, j int) bool {
+		if products[i].Price == products[j].Price {
+			return products[i].Name < products[j].Name
+		}
+		return products[i].Price > products[j].Price
+	})
+
+	result := make([]string, len(products))
+	for i, product := range products {
+		result[i] = product.Name
+	}
+
+	return result
 }
