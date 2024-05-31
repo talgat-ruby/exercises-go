196c83acdab378f01e7470bfd8e18334ef16391e
Artem Prima <a.prima@lerna.digital>
2024-05-31 18:43:35 +0500

    add problem12

--- exercise1/problem12/problem12.go
+++ exercise1/problem12/problem12.go
@@ -1,3 +1,18 @@
 package problem11
 
-func keysAndValues() {}
+func keysAndValues[K comparable, V any](inp map[K]V) ([]K, []V) {
+	var keys []K
+	var values []V
+
+	if inp == nil {
+		return keys, values
+	}
+
+	// Создаем срезы для ключей и значений
+	for key, value := range inp {
+		keys = append(keys, key)
+		values = append(values, value)
+	}
+
+	return keys, values
+}
