80edcfb371738b6fe8a364e4fd5f593c9b953896
Artem Prima <a.prima@lerna.digital>
2024-05-31 16:10:21 +0500

    add problem10

--- exercise1/problem10/problem10.go
+++ exercise1/problem10/problem10.go
@@ -1,3 +1,22 @@
 package problem10
 
-func factory() {}
+func factory() (map[string]int, func(string) func(int)) {
+	// Инициализация карты брендов.
+	brands := make(map[string]int)
+
+	// Определение функции makeBrand для создания инкрементеров брендов.
+	makeBrand := func(brand string) func(int) {
+		// Инициализация счетчика для бренда.
+		counter := 0
+		brands[brand] = counter
+
+		// Возвращаем функцию инкрементации.
+		return func(inc int) {
+			counter += inc
+			brands[brand] = counter
+		}
+	}
+
+	// Возвращаем карту брендов и функцию makeBrand.
+	return brands, makeBrand
+}
