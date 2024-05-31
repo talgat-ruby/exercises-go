3523c01bf0bd852a9566a9b50c3f5fb4a301725a
Artem Prima <a.prima@lerna.digital>
2024-05-31 15:04:27 +0500

    add problem9

--- exercise1/problem9/problem9.go
+++ exercise1/problem9/problem9.go
@@ -1,3 +1,14 @@
 package problem9
 
-func factory() {}
+func factory(num int) func(...int) []int {
+	// Возвращаемая функция принимает переменное количество аргументов типа int и возвращает срез int.
+	return func(args ...int) []int {
+		// Создаем срез для хранения результатов.
+		result := make([]int, len(args))
+		// Заполняем срез, умножая каждый аргумент на число num.
+		for i, arg := range args {
+			result[i] = arg * num
+		}
+		return result
+	}
+}
