85cda11ff692997d8473e4ad7b6d8c66032df0a2
Diana Shapenova <diana@MacBook-Pro-Diana.local>
2024-06-02 00:36:08 +0500

    solved other problems

--- exercise1/problem10/problem10.go
+++ exercise1/problem10/problem10.go
@@ -2,11 +2,16 @@ package problem10
 
 func factory() (map[string]int, func(string) func(int)) {
 	brands := make(map[string]int)
+	i := 0
 
 	makeBrand := func(brand string) func(int) {
-		return func(increment int) {
-			brands[brand] += increment
+		brands[brand] = i
+
+		inc := func(i int) {
+			brands[brand] += i
 		}
+
+		return inc
 	}
 
 	return brands, makeBrand
