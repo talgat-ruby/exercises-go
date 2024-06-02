0851ec1e0d87dd217230f3063592b85055370312
Adi Yeltay <adiyeltay@Adis-MacBook-Pro.local>
2024-06-02 18:54:21 +0500

    finished except for 3 and 6

--- exercise1/problem11/problem11.go
+++ exercise1/problem11/problem11.go
@@ -2,12 +2,13 @@ package problem11
 
 func removeDups[T ~int | ~bool | ~string](items []T) []T {
 	itemsMap := make(map[T]int)
-	for _, item := range items {
-		itemsMap[item] = 0
-	}
 	uniqueItems := []T{}
-	for k, _ := range itemsMap {
-		uniqueItems = append(uniqueItems, k)
+	for _, item := range items {
+		_, found := itemsMap[item]
+		if !found {
+			uniqueItems = append(uniqueItems, item)
+			itemsMap[item] += 1
+		}
 	}
 	return uniqueItems
 }
