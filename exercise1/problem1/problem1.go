ef786060d9fc7aab05722733d3bf984d8a7c714c
daul08 <daul08@mail.ru>
2024-06-02 10:40:04 +0500

    problem1

--- exercise1/problem1/problem1.go
+++ exercise1/problem1/problem1.go
@@ -1,4 +1,14 @@
 package problem1
 
-func isChangeEnough() {
+func isChangeEnough(change []int, amountDue float64) bool {
+	quarterValue := 0.25
+	dimeValue := 0.10
+	nickelValue := 0.05
+	pennyValue := 0.01
+
+	// Рассчитываем общую сумму денег
+	total := float64(change[0])*quarterValue + float64(change[1])*dimeValue + float64(change[2])*nickelValue + float64(change[3])*pennyValue
+
+	// Сравниваем с долгом
+	return total >= amountDue
 }
