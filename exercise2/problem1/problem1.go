043ffff54db0244375268978b6147f08cea12285
Abdykalykov Alnur <abdykalykovalnur@gmail.com>
2024-06-06 14:13:20 +0500

    p1

--- exercise2/problem1/problem1.go
+++ exercise2/problem1/problem1.go
@@ -1,3 +1,35 @@
 package problem1
 
-type Queue struct{}
+import "errors"
+
+type Queue struct {
+	Arr []any
+}
+
+func (q *Queue) Enqueue(elem any) {
+	q.Arr = append(q.Arr, elem)
+}
+
+func (q *Queue) Dequeue() (any, error) {
+	if len(q.Arr) == 0 {
+		return nil, errors.New("queue is empty")
+	}
+	elem := q.Arr[0]
+	q.Arr = q.Arr[1:]
+	return elem, nil
+}
+func (q *Queue) Peek() (any, error) {
+	if len(q.Arr) == 0 {
+		return nil, errors.New("queue is empty")
+	}
+	return q.Arr[0], nil
+}
+func (q *Queue) Size() int {
+	return len(q.Arr)
+}
+func (q *Queue) IsEmpty() bool {
+	if len(q.Arr) == 0 {
+		return true
+	}
+	return false
+}
