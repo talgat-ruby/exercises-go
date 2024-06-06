e00f92e035f235185bca5a1f774fead510099ad1
Abdykalykov Alnur <abdykalykovalnur@gmail.com>
2024-06-06 14:34:46 +0500

    p2

--- exercise2/problem2/problem2.go
+++ exercise2/problem2/problem2.go
@@ -1,3 +1,37 @@
 package problem2
 
-type Stack struct{}
+import "errors"
+
+type Stack struct {
+	Arr []any
+}
+
+func (s *Stack) Push(elem any) {
+	newArr := []any{elem}
+	newArr = append(newArr, s.Arr...)
+	s.Arr = newArr
+}
+func (s *Stack) Pop() (any, error) {
+	if len(s.Arr) == 0 {
+		return nil, errors.New("empty stack")
+	}
+	elem := s.Arr[0]
+	s.Arr = s.Arr[1:]
+	return elem, nil
+}
+func (s *Stack) Peek() (any, error) {
+	if len(s.Arr) == 0 {
+		return nil, errors.New("empty stack")
+	}
+	elem := s.Arr[0]
+	return elem, nil
+}
+func (s *Stack) Size() int {
+	return len(s.Arr)
+}
+func (s *Stack) IsEmpty() bool {
+	if len(s.Arr) == 0 {
+		return true
+	}
+	return false
+}
