f75f0831f51a8c7a4815320fdaf86757d856e070
Abdykalykov Alnur <abdykalykovalnur@gmail.com>
2024-06-07 10:24:38 +0500

    p5

--- exercise2/problem5/problem5.go
+++ exercise2/problem5/problem5.go
@@ -1,3 +1,18 @@
 package problem5
 
-type Person struct{}
+import "fmt"
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+func (p *Person) compareAge(person *Person) string {
+	if p.Age < person.Age {
+		return fmt.Sprintf("%s is older than me.", person.Name)
+	} else if p.Age > person.Age {
+		return fmt.Sprintf("%s is younger than me.", person.Name)
+	}
+	return fmt.Sprintf("%s is the same age as me.", person.Name)
+
+}
