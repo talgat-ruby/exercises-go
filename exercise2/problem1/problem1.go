a7741cdc8f30a0e4c1051de0a2591ca205962bfa
asabiyev <asabiyev@jmart.kz>
2024-06-10 01:19:22 +0500

    first three problems

--- exercise2/problem1/problem1.go
+++ exercise2/problem1/problem1.go
@@ -1,3 +1,44 @@
 package problem1
 
-type Queue struct{}
+import (
+	"errors"
+)
+
+type Queue struct {
+	items []any
+}
+
+func (q *Queue) Enqueue(item any) {
+	q.items = append(q.items, item)
+}
+
+func (q *Queue) Dequeue() (any, error) {
+	if len(q.items) == 0 {
+		err := errors.New("queue is empty. Nothing to dequeue")
+
+		return nil, err
+	}
+
+	cur := q.items[0]
+	q.items = q.items[1:]
+
+	return cur, nil
+}
+
+func (q *Queue) Size() int {
+	return len(q.items)
+}
+
+func (q *Queue) Peek() (any, error) {
+	if len(q.items) == 0 {
+		err := errors.New("queue is empty. Nothing to peek")
+
+		return nil, err
+	}
+
+	return q.items[0], nil
+}
+
+func (q *Queue) IsEmpty() bool {
+	return q.Size() == 0
+}
--- exercise2/problem2/problem2.go
+++ exercise2/problem2/problem2.go
@@ -1,3 +1,42 @@
 package problem2
 
-type Stack struct{}
+import "errors"
+
+type Stack struct {
+	items []any
+}
+
+func (s *Stack) Push(item any) {
+	s.items = append(s.items, item)
+}
+
+func (s *Stack) Pop() (any, error) {
+	if len(s.items) == 0 {
+		err := errors.New("stack is empty. Nothing to pop")
+
+		return nil, err
+	}
+
+	item := s.items[len(s.items)-1]
+	s.items = s.items[:len(s.items)-1]
+
+	return item, nil
+}
+
+func (s *Stack) Size() int {
+	return len(s.items)
+}
+
+func (s *Stack) Peek() (any, error) {
+	if len(s.items) == 0 {
+		err := errors.New("stack is empty. Nothing to peek")
+
+		return nil, err
+	}
+
+	return s.items[len(s.items)-1], nil
+}
+
+func (s *Stack) IsEmpty() bool {
+	return s.Size() == 0
+}
