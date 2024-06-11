54abdc9c1266170bc0da4915d8ca4868a6cb2aee
Adi Yeltay <adiyeltay@Adis-MacBook-Pro.local>
2024-06-11 16:00:12 +0500

    exercise 2 completed, but problem 7 has wrong tests

--- exercise2/problem6/problem6.go
+++ exercise2/problem6/problem6.go
@@ -1,7 +1,24 @@
 package problem6
 
-type Animal struct{}
+type Animal struct {
+	name    string
+	legsNum int
+}
 
-type Insect struct{}
+type Insect struct {
+	name    string
+	legsNum int
+}
 
-func sumOfAllLegsNum() {}
+func sumOfAllLegsNum(creatures ...interface{}) int {
+	sum := 0
+	for _, creature := range creatures {
+		switch v := creature.(type) {
+		case *Animal:
+			sum += v.legsNum
+		case *Insect:
+			sum += v.legsNum
+		}
+	}
+	return sum
+}
