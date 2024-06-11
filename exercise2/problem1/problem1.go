fe53f287e339624a73569822b24a5171bcafbfa4
myrzakhmet <smagul.myrzakhmet@mail.ru>
2024-06-11 16:21:58 +0500

    change test file, because range over int not allowed

--- exercise2/problem1/problem1.go
+++ exercise2/problem1/problem1.go
@@ -1,3 +1,59 @@
 package problem1
 
-type Queue struct{}
+import "fmt"
+
+type Node struct {
+	Data any
+	Tail *Node
+}
+
+type Queue struct {
+	head *Node
+}
+
+func (q *Queue) Size() int {
+	head := q.head
+	size := 0
+	for head != nil {
+		head = head.Tail
+		size++
+	}
+
+	return size
+}
+
+func (q *Queue) IsEmpty() bool {
+	return q.head == nil
+}
+
+func (q *Queue) Enqueue(v any) {
+	node := q.head
+	if node == nil {
+		q.head = &Node{Data: v}
+		return
+	}
+
+	for node.Tail != nil {
+		node = node.Tail
+	}
+	node.Tail = &Node{Data: v}
+}
+
+func (q *Queue) Dequeue() (any, error) {
+	if q.head == nil {
+		return nil, fmt.Errorf("empty queue")
+	}
+
+	v := q.head.Data
+	q.head = q.head.Tail
+	return v, nil
+}
+
+func (q *Queue) Peek() (any, error) {
+	if q.head == nil {
+		return nil, fmt.Errorf("empty queue")
+	}
+
+	v := q.head.Data
+	return v, nil
+}
