c3d05fdb39d5229315192e702d5e4a04ddcbb49c
Sultan <sultansakyp77@gmail.com>
2024-06-11 17:54:55 +0500

    commit on exercise2

--- exercise2/problem2/problem2.go
+++ exercise2/problem2/problem2.go
@@ -1,3 +1,35 @@
 package problem2
 
-type Stack struct{}
+import "errors"
+
+type Stack struct {
+	Elements []any
+}
+
+func (s *Stack) Push(val any) {
+	s.Elements = append(s.Elements, val)
+}
+
+func (s *Stack) Pop() (any, error) {
+	if s.IsEmpty() {
+		return nil, errors.New("stack is empty")
+	}
+	popped := s.Elements[s.Size()-1]
+	s.Elements = s.Elements[:(s.Size() - 1)]
+	return popped, nil
+}
+
+func (s *Stack) Peek() (any, error) {
+	if s.IsEmpty() {
+		return nil, errors.New("stack is empty")
+	}
+	return s.Elements[s.Size()-1], nil
+}
+
+func (s *Stack) IsEmpty() bool {
+	return s.Size() == 0
+}
+
+func (s *Stack) Size() int {
+	return len(s.Elements)
+}
--- exercise2/problem5/problem5.go
+++ exercise2/problem5/problem5.go
@@ -1,3 +1,20 @@
 package problem5
 
-type Person struct{}
+import "fmt"
+
+type Person struct {
+	name string
+	age  int
+}
+
+func (p *Person) compareAge(anotherPerson *Person) (res string) {
+
+	if anotherPerson.age > p.age {
+		res = fmt.Sprintf("%s is older than me.", anotherPerson.name)
+	} else if anotherPerson.age == p.age {
+		res = fmt.Sprintf("%s is the same age as me.", anotherPerson.name)
+	} else {
+		res = fmt.Sprintf("%s is younger than me.", anotherPerson.name)
+	}
+	return res
+}
