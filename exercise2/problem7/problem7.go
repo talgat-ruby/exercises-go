2f9f13a63bb9c0f76373cb27b64839f260109a53
Artem Prima <a.prima@lerna.digital>
2024-06-12 23:43:47 +0500

    problem7 fail

--- exercise2/problem7/problem7.go
+++ exercise2/problem7/problem7.go
@@ -1,10 +1,32 @@
 package problem7
 
+import (
+	"fmt"
+)
+
 type BankAccount struct {
+	name    string
+	balance int
 }
 
 type FedexAccount struct {
+	name     string
+	packages []string
 }
 
 type KazPostAccount struct {
+	name     string
+	balance  int
+	packages []string
+}
+
+func withdrawMoney(amount int, fromAccount BankAccount, toAccount *KazPostAccount) {
+	fromAccount.balance -= amount
+	toAccount.balance += amount
+}
+
+func sendPackagesTo(receiver string, fromAccount FedexAccount, toAccount *KazPostAccount) {
+	packageMessage := fmt.Sprintf("%s send package to %s", fromAccount.name, receiver)
+	fromAccount.packages = append(fromAccount.packages, packageMessage)
+	toAccount.packages = append(toAccount.packages, packageMessage)
 }
