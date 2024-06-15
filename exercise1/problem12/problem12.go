6584f98ebc53e8f85d184b90ec875c3342c1bb06
aidyninho <ryzyk.aidyn@gmail.com>
2024-06-15 18:15:59 +0500

    E-12: Done

--- exercise1/problem12/problem12.go
+++ exercise1/problem12/problem12.go
@@ -1,3 +1,25 @@
 package problem11
 
-func keysAndValues() {}
+import (
+	"golang.org/x/exp/constraints"
+	"sort"
+)
+
+func keysAndValues[K constraints.Ordered, V any](m map[K]V) ([]K, []V) {
+	keys := make([]K, 0)
+	values := make([]V, 0)
+
+	for k := range m {
+		keys = append(keys, k)
+	}
+
+	sort.Slice(keys, func(i, j int) bool {
+		return keys[i] < keys[j]
+	})
+
+	for _, v := range keys {
+		values = append(values, m[v])
+	}
+
+	return keys, values
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/talgat-ruby/exercises-go
 
 go 1.22.3
+
+require golang.org/x/exp v0.0.0-20240613232115-7f521ea00fb8
