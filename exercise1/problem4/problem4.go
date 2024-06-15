fe6a54fc7f01fda1f47201c34c0f6744ecb4c5b4
aidyninho <ryzyk.aidyn@gmail.com>
2024-06-15 13:17:59 +0500

    E-4: Done

--- exercise1/problem4/problem4.go
+++ exercise1/problem4/problem4.go
@@ -1,4 +1,12 @@
 package problem4
 
-func mapping() {
+import "strings"
+
+func mapping(letters []string) map[string]string {
+	m := make(map[string]string, len(letters))
+	for _, letter := range letters {
+		m[letter] = strings.ToUpper(letter)
+	}
+
+	return m
 }
