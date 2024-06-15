999f0df3593b1be7867be927b1d2731c40d73bc5
aidyninho <ryzyk.aidyn@gmail.com>
2024-06-15 17:04:36 +0500

    E-9: Done

--- exercise1/problem9/problem9.go
+++ exercise1/problem9/problem9.go
@@ -1,3 +1,11 @@
 package problem9
 
-func factory() {}
+func factory(size int) func(args ...int) []int {
+	return func(args ...int) []int {
+		s := make([]int, 0)
+		for _, arg := range args {
+			s = append(s, arg*size)
+		}
+		return s
+	}
+}
