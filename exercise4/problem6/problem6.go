f3538eab54f37d897272f2746709888479038f3c
Artem Prima <a.prima@lerna.digital>
2024-07-21 17:52:29 +0500

    ex4 problem6

--- exercise4/problem6/problem6.go
+++ exercise4/problem6/problem6.go
@@ -2,8 +2,34 @@ package problem6
 
 type pipe func(in <-chan int) <-chan int
 
-func multiplyBy2(in <-chan int) <-chan int {}
+// multiplyBy2 умножает каждое число на 2
+func multiplyBy2(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		defer close(out)
+		for n := range in {
+			out <- n * 2
+		}
+	}()
+	return out
+}
 
-func add5(in <-chan int) <-chan int {}
+// add5 добавляет 5 к каждому числу
+func add5(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		defer close(out)
+		for n := range in {
+			out <- n + 5
+		}
+	}()
+	return out
+}
 
-func piper(in <-chan int, pipes []pipe) <-chan int {}
+// piper комбинирует несколько операций (pipes)
+func piper(in <-chan int, pipes []pipe) <-chan int {
+	for _, p := range pipes {
+		in = p(in)
+	}
+	return in
+}
