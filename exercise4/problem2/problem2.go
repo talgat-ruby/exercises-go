50b715aaa982e3ade0cdb5cb2f2919b70f8e0abc
Artem Prima <a.prima@lerna.digital>
2024-07-21 17:46:43 +0500

    ex4 problem2

--- exercise4/problem2/problem2.go
+++ exercise4/problem2/problem2.go
@@ -1,5 +1,10 @@
 package problem2
 
+import (
+	"runtime"
+	"sync"
+)
+
 // add - sequential code to add numbers, don't update it, just to illustrate concept
 func add(numbers []int) int64 {
 	var sum int64
@@ -11,6 +16,33 @@ func add(numbers []int) int64 {
 
 func addConcurrently(numbers []int) int64 {
 	var sum int64
+	numCores := runtime.NumCPU() // Получаем количество доступных ядер
+	runtime.GOMAXPROCS(numCores) // Устанавливаем использование всех ядер
 
+	partSize := (len(numbers) + numCores - 1) / numCores // Определяем размер части
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+
+	for i := 0; i < numCores; i++ {
+		start := i * partSize
+		end := start + partSize
+		if end > len(numbers) {
+			end = len(numbers)
+		}
+		wg.Add(1)
+
+		go func(part []int) {
+			defer wg.Done()
+			var partSum int64
+			for _, n := range part {
+				partSum += int64(n)
+			}
+			mu.Lock()
+			sum += partSum
+			mu.Unlock()
+		}(numbers[start:end])
+	}
+
+	wg.Wait()
 	return sum
 }
