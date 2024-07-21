e54757e122b0f6b3bd9ca21efc8e4daf12a69e52
Artem Prima <a.prima@lerna.digital>
2024-07-21 18:01:47 +0500

    ex4 problem7

--- exercise4/problem7/problem7.go
+++ exercise4/problem7/problem7.go
@@ -1,5 +1,14 @@
 package problem7
 
 func multiplex(ch1 <-chan string, ch2 <-chan string) []string {
-	return []string{}
+	output := make([]string, 0, 2)
+	for i := 0; i < 2; i++ {
+		select {
+		case x := <-ch1:
+			output = append(output, x)
+		case x := <-ch2:
+			output = append(output, x)
+		}
+	}
+	return output
 }
