540cec15ac6d9d63b856c0205de05f719fc4c4a8
Kydyrgazy <otakukz17@gmail.com>
2024-07-28 14:34:33 +0500

    Решены задачи в exercise1

--- exercise1/problem7/main.go
+++ exercise1/problem7/main.go
@@ -1,3 +1,13 @@
 package main
 
-func highestDigit() {}
+func highestDigit(n int) int {
+	highest := 0
+	for n > 0 {
+		digit := n % 10
+		if digit > highest {
+			highest = digit
+		}
+		n /= 10
+	}
+	return highest
+}
