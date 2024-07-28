72d52e132308031bbebf04195612d7f88805e35e
Жантасов Адлет Биржанович <azhantas@gb-m-0008.local>
2024-07-28 19:15:42 +0500

    feature/exercise1

--- exercise1/problem9/main.go
+++ exercise1/problem9/main.go
@@ -1,7 +1,25 @@
 package main
 
-func bitwiseAND() {}
+import (
+	"fmt"
+)
 
-func bitwiseOR() {}
+func main() {
+	a := 6
+	b := 23
+	fmt.Println(bitwiseAND(a, b))
+	fmt.Println(bitwiseOR(a, b))
+	fmt.Println(bitwiseXOR(a, b))
+}
 
-func bitwiseXOR() {}
+func bitwiseAND(a, b int) int {
+	return a & b
+}
+
+func bitwiseOR(a, b int) int {
+	return a | b
+}
+
+func bitwiseXOR(a, b int) int {
+	return a ^ b
+}
