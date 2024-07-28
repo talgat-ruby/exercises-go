61ef07d223114530638f458d140916440c41223c
UAssylbek <154306656+UAssylbek@users.noreply.github.com>
2024-07-28 12:05:06 +0500

    Assylbek Usserbay

--- exercise1/problem8/main.go
+++ exercise1/problem8/main.go
@@ -1,3 +1,17 @@
 package main
 
-func countVowels() {}
+func countVowels(a string) int {
+	res := 0
+
+	for _, i := range a {
+		if vowels(i) {
+			res++
+		}
+	}
+
+	return res 
+}
+
+func vowels(a rune) bool {
+	return a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u' || a == 'A' || a == 'E' || a == 'I' || a == 'O' || a == 'U'
+}
