3e8f9ee5827f569676177ef4c8ac23d467fb622a
Жантасов Адлет Биржанович <azhantas@gb-m-0008.local>
2024-07-28 19:15:42 +0500

    feature/exercise1

--- exercise1/problem3/main.go
+++ exercise1/problem3/main.go
@@ -1,3 +1,18 @@
 package main
 
-func numberSquares() {}
+import "fmt"
+
+func main() {
+	fmt.Println(numberSquares(2))
+	fmt.Println(numberSquares(3))
+	fmt.Println(numberSquares(4))
+	fmt.Println(numberSquares(5))
+}
+
+func numberSquares(n int) int {
+	totalSquares := 0
+	for i := 1; i <= n; i++ {
+		totalSquares += (n - i + 1) * (n - i + 1)
+	}
+	return totalSquares
+}
--- exercise1/problem7/main.go
+++ exercise1/problem7/main.go
@@ -1,3 +1,25 @@
 package main
 
-func highestDigit() {}
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	fmt.Println(highestDigit(379))
+	fmt.Println(highestDigit(2))
+	fmt.Println(highestDigit(377401))
+}
+
+func highestDigit(number int) int {
+	numStr := strconv.Itoa(number)
+	maxDigit := 0
+
+	for _, digit := range numStr {
+		digit, _ := strconv.Atoi(string(digit))
+		if digit > maxDigit {
+			maxDigit = digit
+		}
+	}
+	return maxDigit
+}
