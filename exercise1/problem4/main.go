ebe8ba1db3cf6f3328ad18c060dc9d78ff46cc77
Alinur-rama <alinur@outstaff.rama.gg>
2024-07-29 13:37:56 +0500

    done

--- exercise1/problem4/main.go
+++ exercise1/problem4/main.go
@@ -1,3 +1,13 @@
 package main
 
-func detectWord() {}
+import "unicode"
+
+func detectWord(a string) string {
+	sum := ""
+	for _, char := range a {
+		if unicode.IsLower(char) {
+			sum += string(char)
+		}
+	}
+	return sum
+}
