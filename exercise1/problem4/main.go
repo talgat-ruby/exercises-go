1aec238d2928953bb7074cddb799e840619f5626
esotericbomb <kaisar19166@gmail.com>
2024-07-30 03:49:42 +0500

    norm

--- exercise1/problem4/main.go
+++ exercise1/problem4/main.go
@@ -1,3 +1,25 @@
 package main
 
-func detectWord() {}
+import (
+	"fmt"
+	"unicode"
+)
+
+func detectWord(word string) string {
+	rr := []rune(word)
+	var fw string
+	for i := 0; i < len(word); i++ {
+		if unicode.IsLower(rr[i]) {
+			fw = fw + string(word[i])
+			fmt.Printf("%c", word[i])
+		}
+
+	}
+	return fw
+}
+
+func main() {
+	var b string
+	fmt.Scan(&b)
+	detectWord(b)
+}
