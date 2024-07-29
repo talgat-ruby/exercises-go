b8790ed42f037c2c2ea76224f7973f4ed523f1a9
esotericbomb <kaisar19166@gmail.com>
2024-07-30 02:56:26 +0500

    easy

---
+++ exercise1/problem3/go.mod
@@ -0,0 +1,3 @@
+module github.com/esotericbomb/exercises-go/tree/main/exercise1/problem3
+
+go 1.22.5
--- exercise1/problem3/main.go
+++ exercise1/problem3/main.go
@@ -1,3 +1,18 @@
 package main
 
-func numberSquares() {}
+import "fmt"
+
+func numberSquares(st int) int {
+	if st == 1 {
+		return 1
+	}
+	b := st*st + numberSquares(st-1)
+	return b
+}
+
+func main() {
+	var b int
+	fmt.Scan(&b)
+	res := numberSquares(b)
+	fmt.Print(res)
+}
