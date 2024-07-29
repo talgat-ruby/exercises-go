a781e78aee057bb288482e69e18efac363b52e8c
eraga0423 <eraga0423@gmail.com>
2024-07-29 19:37:20 +0500

    problem1

--- exercise1/problem1/main.go
+++ exercise1/problem1/main.go
@@ -1,3 +1,9 @@
 package main
 
-func addUp() {}
+func addUp(num int) int {
+	var count int
+	for i := 1; i <= num; i++ {
+		count += i
+	}
+	return count
+}
--- exercise1/problem2/main.go
+++ exercise1/problem2/main.go
@@ -1,3 +1,14 @@
 package main
 
-func binary() {}
+func binary(num int) int {
+	a := 0
+	b := 0
+	for num != 0{
+		a = num %2
+		b  = num /2
+		num = b
+
+
+	}
+	return a
+}
