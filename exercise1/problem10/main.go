f1eed0acc1e9a8c03da67db69fc8403509143e47
magzhansabyr <magzhan.sabyr@gmail.com>
2024-07-29 20:00:06 +0500

    commithomework1

--- exercise1/problem10/main.go
+++ exercise1/problem10/main.go
@@ -1,3 +1,23 @@
 package main
 
-func sum() {}
+import (
+	"fmt"
+	"strconv"
+)
+
+// Функция sum суммирует два числа, представленных в виде строк.
+// Если одно из значений не является допустимым числом, возвращает ошибку.
+func sum(a, b string) (string, error) {
+	numA, err := strconv.Atoi(a)
+	if err != nil {
+		return "", fmt.Errorf("string: %s cannot be converted", a)
+	}
+
+	numB, err := strconv.Atoi(b)
+	if err != nil {
+		return "", fmt.Errorf("string: %s cannot be converted", b)
+	}
+
+	result := numA + numB
+	return strconv.Itoa(result), nil
+}
--- exercise1/problem8/main.go
+++ exercise1/problem8/main.go
@@ -1,3 +1,21 @@
 package main
 
-func countVowels() {}
+import (
+	"strings"
+)
+
+// Функция countVowels считает количество гласных букв в строке
+func countVowels(text string) int {
+	vowels := "aeiouAEIOU" // Множество гласных
+	count := 0
+
+	// Пройтись по каждому символу в строке
+	for _, char := range text {
+		// Проверить, является ли символ гласной
+		if strings.ContainsRune(vowels, char) {
+			count++
+		}
+	}
+
+	return count
+}
