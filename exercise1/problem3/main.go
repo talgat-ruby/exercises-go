c51b0dc03db93e75446e690de9f0d8b480d83df0
Baiyrsha <baiyrsha@gmail.com>
2024-07-29 15:27:28 +0500

    Update main.go

--- exercise1/problem3/main.go
+++ exercise1/problem3/main.go
@@ -1,3 +1,22 @@
 package main
 
-func numberSquares() {}
+import (
+	"fmt"
+)
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	totalSquares := numberSquares(n)
+	fmt.Println("Total number of squares in a", n, "x", n, "grid:", totalSquares)
+}
+
+// countSquares calculates the number of different squares in an n * n square grid.
+func numberSquares(n int) int {
+	totalSquares := 0
+	for size := 1; size <= n; size++ {
+		totalSquares += (n - size + 1) * (n - size + 1)
+	}
+	return totalSquares
+}
