18ee1949b441788259d805a1876b48f1691679b2
Beibarus Imacson <bumirbayev@jmart.kz>
2024-07-29 13:03:21 +0500

    solutions for exercise1

--- exercise1/problem3/main.go
+++ exercise1/problem3/main.go
@@ -1,3 +1,11 @@
 package main
 
-func numberSquares() {}
+import "math"
+
+func numberSquares(n int) int {
+	squares := 0
+	for i := 1; i <= n; i++ {
+		squares += int(math.Pow(float64(i), 2))
+	}
+	return squares
+}
--- exercise1/problem6/main.go
+++ exercise1/problem6/main.go
@@ -1,3 +1,25 @@
 package main
 
-func emojify() {}
+import "strings"
+
+var emojiMap = map[string]string{
+	"smile": "🙂",
+	"grin":  "😀",
+	"sad":   "😥",
+	"mad":   "😠",
+}
+
+// Function to replace specific words with emojis
+func emojify(sentence string) string {
+	words := strings.Fields(sentence)
+	for i, word := range words {
+		// Remove punctuation from the word
+		cleanWord := strings.Trim(word, ".,!?;:")
+		if emoji, ok := emojiMap[cleanWord]; ok {
+			words[i] = strings.ReplaceAll(word, cleanWord, emoji)
+		}
+	}
+	return strings.Join(words, " ")
+}
+
+//func emojify() {}
