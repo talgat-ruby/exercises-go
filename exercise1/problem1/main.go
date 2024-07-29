582a1d3d892c1590368cd978f867732e5a779592
Baiyrsha <baiyrsha@gmail.com>
2024-07-29 15:25:39 +0500

    Update main.go

--- exercise1/problem1/main.go
+++ exercise1/problem1/main.go
@@ -1,3 +1,19 @@
 package main
 
-func addUp() {}
+import "fmt"
+ 
+func addUp(a int) int {
+	sum := 0
+	for i := 1; i <= a; i++ {
+		sum += i
+	}
+	return sum
+}
+
+func main() {
+	var b int
+
+	//fmt.Scan(&b)
+	result := addUp(b)
+	fmt.Println("addUp", result)
+}
