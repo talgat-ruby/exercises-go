dc847081a504aaeadbf043d32081684ab99ee3cf
Aslan Kalikov <kh.aslan.sh@gmail.com>
2024-07-30 00:41:36 +0500

    homework

--- exercise1/problem6/main.go
+++ exercise1/problem6/main.go
@@ -1,3 +1,23 @@
 package main
 
-func emojify() {}
+import "strings"
+
+func emojify(s string) string {
+	replacements := map[string]string{
+		"smile": "🙂",
+		"grin":  "😀",
+		"sad":   "😥",
+		"mad":   "😠",
+	}
+
+	words := strings.Fields(s)
+	for i, word := range words {
+		cleanedWord := strings.Trim(word, ",.!?")
+
+		if emoji, exists := replacements[cleanedWord]; exists {
+			words[i] = strings.ReplaceAll(word, cleanedWord, emoji)
+		}
+	}
+
+	return strings.Join(words, " ")
+}
--- exercise1/problem8/main.go
+++ exercise1/problem8/main.go
@@ -1,3 +1,16 @@
 package main
 
-func countVowels() {}
+import "strings"
+
+func countVowels(s string) int {
+	vowels := "aeiou"
+	count := 0
+
+	for _, char := range strings.ToLower(s) {
+		if strings.ContainsRune(vowels, char) {
+			count++
+		}
+	}
+
+	return count
+}
