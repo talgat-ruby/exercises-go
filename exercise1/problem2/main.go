7e3e058206bd730729bbef275a46bc4e040ed212
esotericbomb <kaisar19166@gmail.com>
2024-07-30 01:59:00 +0500

    finally

--- exercise1/problem2/main.go
+++ exercise1/problem2/main.go
@@ -2,31 +2,15 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
-func binary(d int) string {
-	if d == 0 {
-		return "0"
-	}
-
-	var bin string
-	var rem int
-	for d > 0 {
-		rem = d & 2
-		fmt.Print(rem)
-		bin = strconv.Itoa(rem) + bin
-		fmt.Print(bin)
-		d = d / 2
-		fmt.Print(d)
-	}
-	return bin
-
+func binary(de int) int {
+	a := uint64(de)
+	return int(a)
 }
 
 func main() {
-	var b int
-	fmt.Scan(&b)
-	res := binary(b)
-	fmt.Println(res)
+	var c int
+	fmt.Scan(&c)
+	fmt.Printf("%b", c)
 }
