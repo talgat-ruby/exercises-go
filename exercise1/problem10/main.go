3cb883f67036cf1f93fab2be9919f39d70ddc1bb
Olzhas <olzhas.serikbayev@qaj.kz>
2024-07-29 17:22:06 +0500

    solved

--- exercise1/problem10/main.go
+++ exercise1/problem10/main.go
@@ -1,3 +1,17 @@
 package main
 
-func sum() {}
+import "strconv"
+
+func sum(str1 string, str2 string) (string, error) {
+	number1, err := strconv.Atoi(str1)
+	if err != nil {
+		return "", err
+	}
+
+	number2, err := strconv.Atoi(str2)
+	if err != nil {
+		return "", err
+	}
+
+	return strconv.Itoa(number1 + number2), nil
+}
