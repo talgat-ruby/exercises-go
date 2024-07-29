4ba6510d2bd3f6f9266551dcd1ba2b52798cddcc
Dimash <dimashaijarikov@gmail.com>
2024-07-29 18:32:04 +0500

    solved 2 problems

--- exercise1/problem1/main.go
+++ exercise1/problem1/main.go
@@ -1,5 +1,15 @@
 package main
 
-func addUp() {
-	println("Hello world")
+import (
+	"fmt"
+)
+
+func addUp(n int) int {
+	return n * (n + 1) / 2
+}
+
+func main() {
+	fmt.Println(addUp(4))
+	fmt.Println(addUp(13))
+	fmt.Println(addUp(600))
 }
--- exercise1/problem2/main.go
+++ exercise1/problem2/main.go
@@ -1,3 +1,16 @@
 package main
 
-func binary() {}
+import (
+	"fmt"
+	"strconv"
+)
+
+func binary(n int) string {
+	return strconv.FormatInt(int64(n), 2)
+}
+
+func main() {
+	fmt.Println(binary(1))
+	fmt.Println(binary(5))
+	fmt.Println(binary(10))
+}
