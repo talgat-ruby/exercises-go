ed9ea9a657d86c11be52445cdcf8cd8650aa29ee
esotericbomb <kaisar19166@gmail.com>
2024-07-30 01:09:01 +0500

    1

--- exercise1/problem2/main.go
+++ exercise1/problem2/main.go
@@ -1,3 +1,29 @@
 package main
 
-func binary() {}
+import (
+	"fmt"
+	"strconv"
+)
+
+func binary(d int) string {
+	if d == 0 {
+		return "0"
+	}
+
+	var bin string
+	var rem int
+	for d > 0 {
+		rem = d & 2
+		bin = strconv.Itoa(rem) + bin
+		d = d / 2
+	}
+	return bin
+
+}
+
+func main() {
+	var inp int
+	fmt.Scan(%inp)
+	res = binary(a)
+	fmt.Println(res)
+}
