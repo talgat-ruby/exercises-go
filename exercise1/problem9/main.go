0f76d34ef247fa23f78922ba0ebf68c63eb3c231
alljazz-go <olzhas2002@bk.ru>
2024-07-30 17:06:04 +0600

    add solution for exercise1\problem8

--- exercise1/problem9/main.go
+++ exercise1/problem9/main.go
@@ -1,7 +1,27 @@
 package main
 
-func bitwiseAND() {}
+import (
+	"fmt"
+)
 
-func bitwiseOR() {}
+// bitwiseAND function returns the bitwise AND of two integers
+func bitwiseAND(a, b int) int {
+	return a & b
+}
 
-func bitwiseXOR() {}
+// bitwiseOR function returns the bitwise OR of two integers
+func bitwiseOR(a, b int) int {
+	return a | b
+}
+
+// bitwiseXOR function returns the bitwise XOR of two integers
+func bitwiseXOR(a, b int) int {
+	return a ^ b
+}
+
+func main() {
+	// Examples of using the bitwise functions
+	fmt.Printf("bitwiseAND(6, 23) = %08b\n", bitwiseAND(6, 23)) // 00000110
+	fmt.Printf("bitwiseOR(6, 23) = %08b\n", bitwiseOR(6, 23))   // 00010111
+	fmt.Printf("bitwiseXOR(6, 23) = %08b\n", bitwiseXOR(6, 23)) // 00010001
+}
