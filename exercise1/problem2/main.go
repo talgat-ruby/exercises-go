2ee34ff4659887c6fc95a2cd86819b6b51425c62
alljazz-go <olzhas2002@bk.ru>
2024-07-30 16:59:23 +0600

    add solution for exercise1\proble2
    Please enter the commit message for your changes. Lines startingd with '#' will be ignored, and an empty message aborts the commit.

--- exercise1/problem2/main.go
+++ exercise1/problem2/main.go
@@ -1,3 +1,18 @@
 package main
 
-func binary() {}
+import (
+	"fmt"
+	"strconv"
+)
+
+// binary function converts a base-10 integer to its base-2 string representation
+func binary(n int) string {
+	return strconv.FormatInt(int64(n), 2)
+}
+
+func main() {
+	// Примеры использования функции binary
+	fmt.Println(binary(1))  // "1"
+	fmt.Println(binary(5))  // "101"
+	fmt.Println(binary(10)) // "1010"
+}
