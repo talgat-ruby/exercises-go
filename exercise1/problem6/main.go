b280fed204cc7eb5ee3f0dfac5aebb93ea907143
sh.kairatuly <s.kairatuly@documentolog.com>
2024-07-30 18:24:18 +0500

    Kairatuly Shynggyskhan exercises

--- exercise1/problem6/main.go
+++ exercise1/problem6/main.go
@@ -1,3 +1,32 @@
 package main
 
-func emojify() {}
+func emojify(sentence string) string {
+	wordToEmoji := map[string]string{
+		"smile": "🙂",
+		"grin":  "😀",
+		"sad":   "😥",
+		"mad":   "😠",
+	}
+
+	words := []rune(sentence)
+	emojiSentence := ""
+
+	i := 0
+	for i < len(words) {
+		match := false
+		for word, emoji := range wordToEmoji {
+			if i+len(word) <= len(words) && sentence[i:i+len(word)] == word {
+				emojiSentence += emoji
+				i += len(word)
+				match = true
+				break
+			}
+		}
+		if !match {
+			emojiSentence += string(words[i])
+			i++
+		}
+	}
+
+	return emojiSentence
+}
