a5dd21681cdaea0610fbbfd8fa3705fd9689df2f
turaroay <aizada.turarova@bcc.kz>
2024-07-30 17:24:55 +0500

    feat: ex 5

--- exercise1/problem5/main.go
+++ exercise1/problem5/main.go
@@ -1,3 +1,30 @@
 package main
 
-func potatoes() {}
+import "fmt"
+
+func main() {
+	fmt.Println(potatoes("potatoapple"))
+}
+
+func potatoes(s string) int {
+	target := "potato"
+	count := 0
+	targetLen := len(target)
+
+	for i := 0; i <= len(s)-targetLen; {
+		match := true
+		for j := 0; j < targetLen; j++ {
+			if s[i+j] != target[j] {
+				match = false
+				break
+			}
+		}
+		if match {
+			count++
+			i += targetLen
+		} else {
+			i++
+		}
+	}
+	return count
+}
