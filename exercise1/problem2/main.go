2d3ad88994d2ca183bc982e3f01c606036b56312
akabdylk <kabdylkakarnur@gmail.com>
2024-07-30 17:40:25 +0500

    Arnur Kabdylkak(Арнур Кабдылкак) commit

--- exercise1/problem2/main.go
+++ exercise1/problem2/main.go
@@ -1,3 +1,14 @@
 package main
+import (
+	"fmt"
+	"strconv"
+)
 
-func binary() {}
+func binary(n int) string {return strconv.FormatInt(int64(n), 2)
+}
+
+func main() {
+	fmt.Println(binary(1)) 
+	fmt.Println(binary(5))  
+	fmt.Println(binary(10)) 
+}
--- exercise1/problem6/main.go
+++ exercise1/problem6/main.go
@@ -1,3 +1,28 @@
 package main
 
-func emojify() {}
+import (
+	"fmt"
+	"strings"
+)
+
+func emojify(sentence string) string {
+	replacements := map[string]string{
+		"smile": "🙂",
+		"grin":  "😀",
+		"sad":   "😥",
+		"mad":   "😠",
+	}
+
+	for word, emoji := range replacements {
+		sentence = strings.Replace(sentence, word, emoji, -1)
+	}
+
+	return sentence
+}
+
+func main() {
+	fmt.Println(emojify("Make me smile"))
+	fmt.Println(emojify("Make me grin"))
+	fmt.Println(emojify("Make me sad"))
+	fmt.Println(emojify("Make me mad"))
+}
