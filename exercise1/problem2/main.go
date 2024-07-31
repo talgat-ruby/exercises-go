4c2df5b8cf724341d4f1142f368d0c7b6ca811c8
YerkibayevZurab <erkibaev_zurab@mail.ru>
2024-07-31 11:48:05 +0300

    add solution for exercises1

--- exercise1/problem2/main.go
+++ exercise1/problem2/main.go
@@ -1,3 +1,16 @@
 package main
 
-func binary() {}
+import (
+	"fmt"
+	"strconv"
+)
+
+func binary(decimal int) string {
+	return strconv.FormatInt(int64(decimal), 2)
+}
+
+func main() {
+	fmt.Println(binary(1))
+	fmt.Println(binary(5))
+	fmt.Println(binary(10))
+}
