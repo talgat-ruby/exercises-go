7cd4440251bfb69b229e30370bf26b1958a739b3
Rakhim Tulyashev <rahim_97@bk.ru>
2024-07-31 17:34:48 +0500

    fix 4 problem и добавил student.txt

--- exercise1/problem4/main.go
+++ exercise1/problem4/main.go
@@ -1,27 +1,13 @@
 package main
 
-import (
-	"fmt"
-)
+import "unicode"
 
-func detectWord(crowd, word string) string {
-	result := ""
-	wordIndex := 0
-	for _, char := range crowd {
-		if char == rune(word[wordIndex]) {
-			result += string(char)
-			wordIndex++
-			if wordIndex == len(word) {
-				break
-			}
+func detectWord(s string) string {
+	word := ""
+	for _, char := range s {
+		if unicode.IsLower(char) {
+			word += string(char)
 		}
 	}
-	return result
+	return word
 }
-func main() {
-	fmt.Println(detectWord("UcUNFYGaFYFYGtNUH"))
-	fmt.Println(detectWord("bEEFGBuFBRrHgUHlNFYaYr"))
-	fmt.Println(detectWord("YFemHUFBbezFBYzFBYLleGBYEFGBMENTment"))
-}
-
-//##не совсем понял где ошибка и в отладке пытался понять,смотреть и вообще нормально ли то что я все гуглю где только возможно, используя все возможности интернета? т.к интенсив и вроде как вводный курс для новичков(p.s Меняю проф деятельтность),но голова ломается при решений, понятно что не ничего понятно. Но все это очень интересно,хоть и не совсем понятно.
