25dd7ed65032a1c3472c6a05fbe875deab31636a
adminIL <admin@IMAC-3-13.local>
2024-07-31 11:54:37 +0500

    Finished hw

--- exercise1/problem10/main.go
+++ exercise1/problem10/main.go
@@ -1,3 +1,21 @@
 package main
 
-func sum() {}
+import (
+	"fmt"
+	"strconv"
+)
+
+func sum(str1, str2 string) (string, error) {
+	num1, err1 := strconv.Atoi(str1)
+	if err1 != nil {
+		return "", fmt.Errorf("string: %s cannot be converted", str1)
+	}
+
+	num2, err2 := strconv.Atoi(str2)
+	if err2 != nil {
+		return "", fmt.Errorf("string: %s cannot be converted", str2)
+	}
+
+	result := strconv.Itoa(num1 + num2)
+	return result, nil
+}
--- exercise1/problem6/main.go
+++ exercise1/problem6/main.go
@@ -1,5 +1,18 @@
 package main
 
+import "strings"
+
 func emojify(str string) string {
-	
+	emojiMap := map[string]string{
+		"smile": "🙂",
+		"grin":  "😀",
+		"sad":   "😥",
+		"mad":   "😠",
+	}
+
+	for word, emoji := range emojiMap {
+		str = strings.Replace(str, word, emoji, -1)
+	}
+
+	return str
 }
