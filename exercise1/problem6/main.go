3b4fc0850426f3cd506e3e73282d9f87481a9fe4
Aliasker <bluematchawithvanillasirup@gmail.com>
2024-07-31 05:46:50 +0500

    Решены задачи будущим гофером в exercises1

--- exercise1/problem6/main.go
+++ exercise1/problem6/main.go
@@ -1,3 +1,32 @@
 package main
 
-func emojify() {}
+import (
+	"fmt"
+	"strings"
+)
+
+// emojify function replaces specific words with their corresponding emojis
+func emojify(s string) string {
+	replacements := map[string]string{
+		"smile": "🙂",
+		"grin":  "😀",
+		"sad":   "😥",
+		"mad":   "😠",
+	}
+
+	words := strings.Split(s, " ")
+	for i, word := range words {
+		if emoji, exists := replacements[word]; exists {
+			words[i] = emoji
+		}
+	}
+
+	return strings.Join(words, " ")
+}
+
+func main() {
+	fmt.Println(emojify("Make me smile"))
+	fmt.Println(emojify("Make me grin"))
+	fmt.Println(emojify("Make me sad"))
+	fmt.Println(emojify("Make me mad"))
+}
--- exercise1/problem7/main.go
+++ exercise1/problem7/main.go
@@ -1,3 +1,28 @@
 package main
 
-func highestDigit() {}
+import (
+	"fmt"
+	"strconv"
+)
+
+func highestDigit(n int) int {
+	numStr := strconv.Itoa(n)
+	highest := 0
+
+	for _, char := range numStr {
+
+		digit, _ := strconv.Atoi(string(char))
+
+		if digit > highest {
+			highest = digit
+		}
+	}
+
+	return highest
+}
+
+func main() {
+	fmt.Println(highestDigit(379))
+	fmt.Println(highestDigit(2))
+	fmt.Println(highestDigit(377401))
+}
