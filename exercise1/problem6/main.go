88f8500165e82cf8940271746e1bbf0240453ca3
esotericbomb <kaisar19166@gmail.com>
2024-07-31 15:05:58 +0500

    norm

--- exercise1/problem6/main.go
+++ exercise1/problem6/main.go
@@ -1,3 +1,31 @@
 package main
 
-func emojify() {}
+import (
+	"fmt"
+	"strings"
+)
+
+func emojify(text string) string {
+	emojiMap := map[string]string{
+		"smile": "🙂",
+		"grin":  "😀",
+		"sad":   "😥",
+		"mad":   "😠",
+	}
+
+	words := strings.Fields(text)
+	for i := 0; i < len(words); i++ {
+		word := strings.ToLower(words[i])
+		if emoji, ok := emojiMap[word]; ok {
+			words[i] = emoji
+		}
+	}
+
+	return strings.Join(words, " ")
+}
+
+func main() {
+	var c string
+	fmt.Scan(&c)
+	emojify(c)
+}
