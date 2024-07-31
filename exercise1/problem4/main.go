63dc700aa27984de1cbd6f2a40a94ba682c89b58
yermek <narimanov.y@kazinsys.kz>
2024-07-31 14:49:18 +0500

    Yermek Narimanov exercises

--- exercise1/problem4/main.go
+++ exercise1/problem4/main.go
@@ -1,3 +1,13 @@
 package main
 
-func detectWord() {}
+import "unicode"
+
+func detectWord(mixedStr string) string {
+    var hiddenWord []rune
+    for _, char := range mixedStr {
+        if unicode.IsLower(char) {
+            hiddenWord = append(hiddenWord, char)
+        }
+    }
+    return string(hiddenWord)
+}
