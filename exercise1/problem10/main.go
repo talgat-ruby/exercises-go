42d0542e042196ab9c493690c51fa4aeb476f977
addazziia <131895068+addazziia@users.noreply.github.com>
2024-07-31 22:46:58 +0500

    kapatayeva-aizada

--- exercise1/problem10/main.go
+++ exercise1/problem10/main.go
@@ -1,3 +1,32 @@
 package main
 
-func sum() {}
+import (
+	"fmt"
+	"strconv"
+)
+
+func sum(a, b string) (string, error) {
+	numA, errA := strconv.Atoi(a)
+	if errA != nil {
+		return "", fmt.Errorf("string: %s cannot be converted", a)
+	}
+
+	numB, errB := strconv.Atoi(b)
+	if errB != nil {
+		return "", fmt.Errorf("string: %s cannot be converted", b)
+	}
+
+	sum := numA + numB
+	return strconv.Itoa(sum), nil
+}
+
+func main() {
+	result, err := sum("1", "2")
+	fmt.Println(result, err)
+
+	result, err = sum("10", "20")
+	fmt.Println(result, err)
+
+	result, err = sum("a", "2")
+	fmt.Println(result, err)
+}
