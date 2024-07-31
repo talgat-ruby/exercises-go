efd743192f602ee3e068b64bf524669e4e0bb561
Vladislav Zernov <zvu.ast@gmail.com>
2024-07-31 15:55:42 +0500

    add solution for exercise1/problem6

--- exercise1/problem6/main.go
+++ exercise1/problem6/main.go
@@ -1,3 +1,24 @@
 package main
 
-func emojify() {}
+import (
+	"fmt"
+	"html"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	xx := "\\U0001f47f"
+
+	// Hex String
+	h := strings.ReplaceAll(xx, "\\U", "0x")
+
+	// Hex to Int
+	i, _ := strconv.ParseInt(h, 0, 64)
+
+	// Unescape the string (HTML Entity -> String).
+	str := html.UnescapeString(string(i))
+
+	// Display the emoji.
+	fmt.Println(str)
+}
