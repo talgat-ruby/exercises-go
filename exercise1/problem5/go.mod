b06accb225f61339b30522ff0b4c0857de2a1e93
esotericbomb <kaisar19166@gmail.com>
2024-07-31 14:32:20 +0500

    norm

---
+++ exercise1/problem5/go.mod
@@ -0,0 +1,3 @@
+module github.com/esotericbomb/exercises-go/tree/main/exercise1/problem5
+
+go 1.22.5
--- exercise1/problem5/main.go
+++ exercise1/problem5/main.go
@@ -1,3 +1,19 @@
 package main
 
-func potatoes() {}
+import (
+	"fmt"
+	"strings"
+)
+
+func potatoes(a string) int {
+	word := "potato"
+	var b int
+	b, _ = fmt.Println(strings.Count(a, word))
+	return b
+}
+
+func main() {
+	var c string
+	fmt.Scan(&c)
+	potatoes(c)
+}
