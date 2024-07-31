da18c5740468da9a2997e756481ea0376aa54c46
Ayan Toleu <ayan_toleu@mail.ru>
2024-07-31 17:08:46 +0500

    Ayan Toleu solving exercise1

--- exercise1/problem1/main.go
+++ exercise1/problem1/main.go
@@ -1,3 +1,9 @@
 package main
 
-func addUp() {}
+func addUp(num int) int {
+	result := 0
+	for i := 1; i <= num; i++ {
+		result += i
+	}
+	return result
+}
--- exercise1/problem7/main.go
+++ exercise1/problem7/main.go
@@ -1,3 +1,17 @@
 package main
 
-func highestDigit() {}
+import "strconv"
+
+func highestDigit(number int) int {
+	maxDigit := 0
+	numStr := strconv.Itoa(number)
+
+	for _, digit := range numStr {
+		digitInt := int(digit - '0') // Convert rune to int
+		if digitInt > maxDigit {
+			maxDigit = digitInt
+		}
+	}
+
+	return maxDigit
+}
