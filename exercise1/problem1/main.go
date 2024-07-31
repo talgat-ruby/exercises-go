a52932f3fde9a332171edbab29ab117a288102dd
ldossymg <lurdssmgl@gmail.com>
2024-07-31 22:43:19 +0500

    main.go

--- exercise1/problem1/main.go
+++ exercise1/problem1/main.go
@@ -2,7 +2,6 @@ package main
 
 import "fmt"
 
-// Функция, которую вы уже написали
 func addUp(n int) int {
     sum := 0
     for i := 1; i <= n; i++ {
@@ -11,9 +10,7 @@ func addUp(n int) int {
     return sum
 }
 
-// Функция main, которая является точкой входа в программу
 func main() {
-    // Пример использования функции addUp
     result := addUp(10)
     fmt.Println("The sum is:", result)
 }
