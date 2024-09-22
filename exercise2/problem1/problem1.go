4948034ce03f305d0c80f94f13659c4a1fc2c245
Meirbek <meirbekrakhmatulla@gmail.com>
2024-09-22 17:50:58 +0500

    solution problem1

--- exercise2/problem1/problem1.go
+++ exercise2/problem1/problem1.go
@@ -1,4 +1,14 @@
 package problem1
 
-func isChangeEnough() {
+func isChangeEnough(amount float64, coins []int) bool {
+	cents := []int{25, 10, 5, 1}
+
+	amountCents := int(amount * 100)
+
+	totalCent := 0
+
+	for i, count := range coins {
+		totalCent += count * cents[i]
+	}
+	return totalCent >= amountCents
 }
