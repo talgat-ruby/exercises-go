5868a6f18667603087e2c5dcbab3c5cf4aede879
Meirbek <meirbekrakhmatulla@gmail.com>
2024-09-22 18:26:12 +0500

    solution problem1

--- exercise2/problem1/problem1.go
+++ exercise2/problem1/problem1.go
@@ -1,14 +1,12 @@
 package problem1
 
-func isChangeEnough(amount float64, coins []int) bool {
-	cents := []int{25, 10, 5, 1}
+func isChangeEnough(changes [4]int, total float32) bool {
+	cent := []int{25, 10, 5, 1}
 
-	amountCents := int(amount * 100)
-
-	totalCent := 0
-
-	for i, count := range coins {
-		totalCent += count * cents[i]
+	sumCent := int(total * 100)
+	result := 0
+	for i, count := range changes {
+		result += count * cent[i]
 	}
-	return totalCent >= amountCents
+	return result >= sumCent
 }
