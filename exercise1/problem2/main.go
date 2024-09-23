8c38b86b37738caccb6255a0d008e46c4996ac7d
Meirbek <meirbekrakhmatulla@gmail.com>
2024-09-23 17:06:17 +0500

    solution problem4

--- exercise1/problem2/main.go
+++ exercise1/problem2/main.go
@@ -1,3 +1,15 @@
 package main
 
-func binary() {}
+import (
+	"strconv"
+)
+
+func binary(decimalStr string) (string, error) {
+	decimal, err := strconv.Atoi(decimalStr)
+	if err != nil {
+		return "", err
+	}
+
+	binaryStr := strconv.FormatInt(int64(decimal), 2)
+	return binaryStr, nil
+}
