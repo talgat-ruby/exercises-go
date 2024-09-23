6c0082e0fe213daa42ec9327b4d08b7cfcd39fed
Saba <s.kurmashev@jusan.kz>
2024-09-23 20:51:15 +0500

    exercise 2 sabit-kurmashev

--- exercise2/problem3/problem3.go
+++ exercise2/problem3/problem3.go
@@ -9,5 +9,21 @@ const (
 	lr dir = "lr"
 )
 
-func diagonalize() {
+func diagonalize(size int, d dir) [][]int {
+	out := make([][]int, size)
+	for i := 0; i < size; i++ {
+		out[i] = make([]int, size)
+		for j := 0; j < size; j++ {
+			if d == ul {
+				out[i][j] = i + j
+			} else if d == ur {
+				out[i][j] = size + i - j - 1
+			} else if d == ll {
+				out[i][j] = size - i + j - 1
+			} else {
+				out[i][j] = 2*size - i - j - 2
+			}
+		}
+	}
+	return out
 }
