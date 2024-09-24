aee8a9ad9d38d7195b90f64937a37ac12b8b5eb4
Alinurmyrzakhanov <alinur.myrzakhanov@gmail.com>
2024-09-24 13:54:57 +0500

    Alinur Myrzakhanov

--- exercise1/problem3/main.go
+++ exercise1/problem3/main.go
@@ -1,3 +1,9 @@
 package main
 
-func numberSquares() {}
+func numberSquares(n int) int {
+	squares := 0
+	for i := 1; i <= n; i++ {
+		squares += (n - i + 1) * (n - i + 1)
+	}
+	return squares
+}
--- exercise2/problem1/problem1.go
+++ exercise2/problem1/problem1.go
@@ -1,4 +1,17 @@
 package problem1
 
-func isChangeEnough() {
+func isChangeEnough(change [4]int, amountDue float32) bool {
+	quartersValue := 25
+	dimesValue := 10
+	nickelsValue := 5
+	pennieValue := 1
+
+	totalCents := (change[0] * quartersValue) +
+		(change[1] * dimesValue) +
+		(change[2] * nickelsValue) +
+		(change[3] * pennieValue)
+
+	amountDueCents := int(amountDue * 100)
+
+	return totalCents >= amountDueCents
 }
