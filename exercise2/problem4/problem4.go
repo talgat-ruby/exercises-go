fc69b77188d64ce3ecb496521b1ef69f90bff474
Askar Imagnali <imangali950@gmail.com>
2024-09-26 00:46:13 +0500

    feature: finished exercise 2

--- exercise2/problem4/problem4.go
+++ exercise2/problem4/problem4.go
@@ -1,4 +1,13 @@
 package problem4
 
-func mapping() {
+import "strings"
+
+func mapping(letters []string) map[string]string {
+	mp := make(map[string]string)
+
+	for _, l := range letters {
+		mp[strings.ToLower(l)] = strings.ToUpper(l)
+	}
+
+	return mp
 }
