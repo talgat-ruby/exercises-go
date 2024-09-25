fff39fab55b03a5fc223586eff77af9db1a4a2cd
Жантасов Адлет Биржанович <azhantas@gb-m-0008.local>
2024-09-26 01:35:12 +0500

    feature/exercise2

--- exercise2/problem1/problem1.go
+++ exercise2/problem1/problem1.go
@@ -1,4 +1,12 @@
 package problem1
 
-func isChangeEnough() {
+func isChangeEnough(values [4]int, totalValue float32) bool {
+	quarters := float32(values[0]) * 0.25
+	dimes := float32(values[1]) * 0.10
+	nickels := float32(values[2]) * 0.05
+	penny := float32(values[3]) * 0.01
+
+	total := quarters + dimes + nickels + penny
+
+	return total >= totalValue
 }
--- exercise2/problem10/problem10.go
+++ exercise2/problem10/problem10.go
@@ -1,3 +1,17 @@
 package problem10
 
-func factory() {}
+func factory() (map[string]int, func(string) func(int)) {
+	brands := make(map[string]int)
+
+	makeBrand := func(brand string) func(int) {
+		if _, exists := brands[brand]; !exists {
+			brands[brand] = 0
+		}
+
+		return func(amount int) {
+			brands[brand] += amount
+		}
+	}
+
+	return brands, makeBrand
+}
