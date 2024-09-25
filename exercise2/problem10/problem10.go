2745485c08c3a56c0df1b1a232818a9cdbec3df4
RuslanKambarov <kambarov.rs@gmail.com>
2024-09-25 20:54:46 +0500

    feat: exercises 2-1

--- exercise2/problem10/problem10.go
+++ exercise2/problem10/problem10.go
@@ -1,3 +1,12 @@
 package problem10
 
-func factory() {}
+func factory() (map[string]int, func(string) func(int)) {
+	m := map[string]int{}
+	f := func(model string) func(int) {
+		m[model] = 0
+		return func(inc int) {
+			m[model] += inc
+		}
+	}
+	return m, f
+}
