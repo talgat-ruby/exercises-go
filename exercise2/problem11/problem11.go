8d05defba0c7e64dbb9206af03a23d99ab883e18
Жантасов Адлет Биржанович <azhantas@gb-m-0008.local>
2024-09-26 01:35:12 +0500

    feature/exercise2

--- exercise2/problem11/problem11.go
+++ exercise2/problem11/problem11.go
@@ -1,3 +1,14 @@
 package problem11
 
-func removeDups() {}
+func removeDups[T comparable](items []T) []T {
+	unique := make(map[T]bool)
+	var result []T
+
+	for _, item := range items {
+		if !unique[item] {
+			unique[item] = true
+			result = append(result, item)
+		}
+	}
+	return result
+}
