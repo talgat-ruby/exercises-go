7e0ca679356d9a75274a4c454a7aadb9fab31d8b
Meirbek <meirbekrakhmatulla@gmail.com>
2024-09-25 15:04:42 +0500

    add solution exercise2 / problem6

--- exercise2/problem6/problem6.go
+++ exercise2/problem6/problem6.go
@@ -1,4 +1,17 @@
 package problem6
 
-func sumOfTwo() {
+func sumOfTwo(a []int, b []int, v int) bool {
+	set := make(map[int]bool)
+
+	for _, num := range a {
+		set[v-num] = true
+	}
+
+	for _, num := range b {
+		if set[num] {
+			return true
+		}
+	}
+
+	return false
 }
