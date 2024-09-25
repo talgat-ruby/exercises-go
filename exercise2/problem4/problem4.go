8c2c695fcd1f3f21a52c72b9c41142c8b653da6c
Жантасов Адлет Биржанович <azhantas@gb-m-0008.local>
2024-09-26 01:35:12 +0500

    feature/exercise2

--- exercise2/problem4/problem4.go
+++ exercise2/problem4/problem4.go
@@ -1,4 +1,11 @@
 package problem4
 
-func mapping() {
+import "strings"
+
+func mapping(letters []string) map[string]string {
+	customMap := make(map[string]string)
+	for _, letter := range letters {
+		customMap[letter] = strings.ToUpper(string(letter))
+	}
+	return customMap
 }
