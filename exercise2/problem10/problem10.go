f25ff8c66c0deb10547fc7db65944a2beaf4a9f7
zharkynai <sushshiit@gmail.com>
2024-09-25 17:36:04 +0500

    exercises

--- exercise2/problem10/problem10.go
+++ exercise2/problem10/problem10.go
@@ -1,3 +1,13 @@
 package problem10
 
-func factory() {}
+func factory() (map[string]int, func(string) func(int)) {
+	brands := make(map[string]int)
+
+	makeB := func(brand string) func(int) {
+		return func(count int) {
+			brands[brand] += count
+		}
+	}
+
+	return brands, makeB
+}
--- exercise2/problem5/problem5.go
+++ exercise2/problem5/problem5.go
@@ -1,4 +1,20 @@
 package problem5
 
-func products() {
+import "sort"
+
+func products(prices map[string]int, minimum int) []string {
+	var result []string
+
+	for product, price := range prices {
+		if price >= minimum {
+			result = append(result, product)
+		}
+	}
+
+	sort.Slice(result, func(i, j int) bool {
+		return prices[result[i]] < prices[result[j]] ||
+			(prices[result[i]] == prices[result[j]] && result[i] < result[j])
+	})
+
+	return result
 }
