56132115c76bced427dace46fa2cfa8683e52d3a
unknown <ara.nanan@yandex.ru>
2024-09-26 23:34:41 +0500

    solved exercise 2

--- exercise2/problem3/problem3.go
+++ exercise2/problem3/problem3.go
@@ -1,13 +1,38 @@
 package main
 
-type dir string
+func diagonalize(n int, corner string) [][]int {
 
-const (
-	ul dir = "ul"
-	ur dir = "ur"
-	ll dir = "ll"
-	lr dir = "lr"
-)
+	matrix := make([][]int, n)
+	for i := range matrix {
+		matrix[i] = make([]int, n)
+	}
 
-func diagonalize() {
+	switch corner {
+	case "ul":
+		for r := 0; r < n; r++ {
+			for c := 0; c < n; c++ {
+				matrix[r][c] = r + c
+			}
+		}
+	case "ur":
+		for r := 0; r < n; r++ {
+			for c := 0; c < n; c++ {
+				matrix[r][c] = (n - 1 + r) - c
+			}
+		}
+	case "ll":
+		for r := 0; r < n; r++ {
+			for c := 0; c < n; c++ {
+				matrix[r][c] = (n - 1 - r) + c
+			}
+		}
+	case "lr":
+		for r := 0; r < n; r++ {
+			for c := 0; c < n; c++ {
+				matrix[r][c] = (n - 1 - c) + (n - 1 - r)
+			}
+		}
+	}
+
+	return matrix
 }
