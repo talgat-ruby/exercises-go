cba29b47e2e928b0f856a1232a029a11f6587345
TheIceEagle <blacklight312@gmail.com>
2024-09-30 16:26:09 +0500

    Exrcise 2 done

--- exercise2/problem12/problem12.go
+++ exercise2/problem12/problem12.go
@@ -1,3 +1,26 @@
-package problem11
+package problem12
 
-func keysAndValues() {}
+import (
+	"cmp"
+	"slices"
+)
+
+func keysAndValues[K cmp.Ordered, V any](m map[K]V) ([]K, []V) {
+	if m == nil {
+		return []K{}, []V{}
+	}
+
+	keys := make([]K, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+
+	slices.Sort(keys)
+
+	values := make([]V, len(keys))
+	for i, k := range keys {
+		values[i] = m[k]
+	}
+
+	return keys, values
+}
