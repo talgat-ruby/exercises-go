c139ef80d5a54488f539c30a601f18f1984d03a7
UAssylbek <154306656+UAssylbek@users.noreply.github.com>
2024-10-01 16:33:37 +0500

    Assylbek Usserbay exercise3

--- exercise3/problem6/problem6.go
+++ exercise3/problem6/problem6.go
@@ -1,7 +1,21 @@
 package problem6
 
-type Animal struct{}
+type Animal struct{
+	name string
+	legsNum int
+}
 
-type Insect struct{}
+type Insect struct{
+	name string
+	legsNum int
+}
 
-func sumOfAllLegsNum() {}
+func sumOfAllLegsNum(horse, kangaroo *Animal, ant, spider *Insect) int {
+	count := 0
+
+	count += horse.legsNum
+	count += kangaroo.legsNum
+	count += ant.legsNum
+	count += spider.legsNum
+	return count
+}
