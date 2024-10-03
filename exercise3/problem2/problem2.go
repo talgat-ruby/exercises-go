55622eb6617f5870cce06822cf16e2e332d96154
Saba <s.kurmashev@jusan.kz>
2024-10-03 16:58:32 +0500

    exercise 3 sabit-kurmashev

--- exercise3/problem2/problem2.go
+++ exercise3/problem2/problem2.go
@@ -1,3 +1,38 @@
 package problem2
 
-type Stack struct{}
+import "errors"
+
+type Stack struct {
+	arr  []any
+	size int
+}
+
+func (s *Stack) Push(val any) {
+	s.arr = append(s.arr, val)
+	s.size++
+}
+
+func (s *Stack) Pop() (any, error) {
+	if s.size <= 0 {
+		return nil, errors.New("stack is empty")
+	}
+	val := s.arr[s.size-1]
+	s.arr = s.arr[:s.size-1]
+	s.size--
+	return val, nil
+}
+
+func (s *Stack) Size() int {
+	return s.size
+}
+
+func (s *Stack) IsEmpty() bool {
+	return s.size == 0
+}
+
+func (s *Stack) Peek() (any, error) {
+	if s.size <= 0 {
+		return nil, errors.New("stack is empty")
+	}
+	return s.arr[s.size-1], nil
+}
