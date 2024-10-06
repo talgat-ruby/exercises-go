3938fb0b22ac78cc0279e7aa9aa5fb6a7ed05fdf
RuslanKambarov <kambarov.rs@gmail.com>
2024-10-07 01:29:03 +0500

    Ruslan.Kambarov Exercise 3

--- exercise3/problem6/problem6.go
+++ exercise3/problem6/problem6.go
@@ -1,7 +1,31 @@
 package problem6
 
-type Animal struct{}
+type creature interface {
+	GetLegs() int
+}
 
-type Insect struct{}
+func (a *Animal) GetLegs() int {
+	return a.legsNum
+}
 
-func sumOfAllLegsNum() {}
+type Animal struct {
+	name    string
+	legsNum int
+}
+
+func (i *Insect) GetLegs() int {
+	return i.legsNum
+}
+
+type Insect struct {
+	name    string
+	legsNum int
+}
+
+func sumOfAllLegsNum(s ...creature) int {
+	var acc int = 0
+	for _, v := range s {
+		acc += v.GetLegs()
+	}
+	return acc
+}
