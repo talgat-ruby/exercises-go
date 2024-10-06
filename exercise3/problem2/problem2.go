3a8d4dfd26f56172929d137203f4db7fa378fc72
Askar Imagnali <imangali950@gmail.com>
2024-10-06 19:34:56 +0500

    feature: finished exercise 3

--- exercise3/problem2/problem2.go
+++ exercise3/problem2/problem2.go
@@ -1,3 +1,35 @@
 package problem2
 
-type Stack struct{}
+import "errors"
+
+type Stack struct {
+	values []any
+}
+
+func (s *Stack) Push(value any) {
+	s.values = append(s.values, value)
+}
+
+func (s *Stack) Pop() (any, error) {
+	if s.IsEmpty() {
+		return nil, errors.New("Stack is empty")
+	}
+	valueToRemove := s.values[s.Size()-1]
+	s.values = s.values[:s.Size()-1]
+	return valueToRemove, nil
+}
+
+func (s *Stack) Peek() (any, error) {
+	if s.IsEmpty() {
+		return nil, errors.New("Stack is empty")
+	}
+	return s.values[s.Size()-1], nil
+}
+
+func (s *Stack) Size() int {
+	return len(s.values)
+}
+
+func (s *Stack) IsEmpty() bool {
+	return s.Size() == 0
+}
