ddf48daa7857d3c74cfe4364cfbde89f84d4cd62
galyym <ksgalym@mail.ru>
2024-10-07 03:04:19 +0500

    Kemalatdin Galym exercise 3

--- exercise3/problem1/problem1.go
+++ exercise3/problem1/problem1.go
@@ -1,3 +1,33 @@
 package problem1
 
-type Queue struct{}
+import "slices"
+
+type Queue struct {
+	vals []any
+}
+
+func (q *Queue) Enqueue(val any) {
+	q.vals = append(q.vals, val)
+}
+
+func (q *Queue) Dequeue() ([]any, error) {
+	valLen := len(q.vals)
+	q.vals = slices.Delete(q.vals, valLen-1, valLen)
+	return q.vals, nil
+}
+
+func (q *Queue) Peek() any {
+	valLen := len(q.vals) - 1
+	return q.vals[valLen]
+}
+
+func (q *Queue) Size() int {
+	return len(q.vals)
+}
+
+func (q *Queue) IsEmpty() bool {
+	if len(q.vals) > 0 {
+		return false
+	}
+	return true
+}
--- main.go
+++ main.go
@@ -1,1 +1,72 @@
 package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+type Queue struct {
+	vals []any
+}
+
+func (q *Queue) Enqueue(val any) {
+	q.vals = append(q.vals, val)
+}
+
+func (q *Queue) Dequeue() (any, error) {
+	valLen := len(q.vals)
+	queueElement := q.vals[valLen-1]
+	q.vals = slices.Delete(q.vals, valLen-1, valLen)
+	return queueElement, nil
+}
+
+func (q *Queue) Peek() any {
+	valLen := len(q.vals) - 1
+	return q.vals[valLen]
+}
+
+func (q *Queue) Size() int {
+	return len(q.vals)
+}
+
+func (q *Queue) IsEmpty() bool {
+	if len(q.vals) > 0 {
+		return false
+	}
+	return true
+}
+
+func main() {
+	queue := Queue{}
+	queue.Enqueue(1)
+	queue.Enqueue(5)
+	queue.Enqueue(9)
+	fmt.Println(queue)
+	queue.Dequeue()
+	fmt.Println(queue)
+	e := queue.Peek()
+	fmt.Println(e)
+	size := queue.Size()
+	fmt.Println(size)
+	fmt.Println(queue.IsEmpty())
+
+	table := []struct {
+		vals []any
+	}{
+		{[]any{1, 2, 3}},
+		{[]any{"1", "2", "3", "4"}},
+		{[]any{true, false}},
+	}
+
+	for _, r := range table {
+		for _, v := range r.vals {
+			queue.Enqueue(v)
+		}
+		for i := range r.vals {
+			d, _ := queue.Dequeue()
+			if d != r.vals[i] {
+				fmt.Println("Error")
+			}
+		}
+	}
+}
