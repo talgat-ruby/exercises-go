fbadae911b2885d3fb8582659385b89b6d7f58ee
Adlet <adlet.zh19966@gmail.com>
2024-10-06 23:21:30 +0600

    feature/exercise3

--- exercise3/problem5/problem5.go
+++ exercise3/problem5/problem5.go
@@ -1,3 +1,16 @@
 package problem5
 
-type Person struct{}
+type Person struct {
+	Name string
+	Age  int
+}
+
+func (p *Person) compareAge(value *Person) string {
+	if p.Age < value.Age {
+		return value.Name + " is older than me."
+	} else if p.Age > value.Age {
+		return value.Name + " is younger than me."
+	} else {
+		return value.Name + " is the same age as me."
+	}
+}
