a3b333dbf709e47bf4842f8a854dc1ca747e0620
Yerkibayev Zurab <YerkibayevZurab>
2024-10-06 20:04:26 +0500

    add solution for exercise3

--- exercise3/problem5/problem5.go
+++ exercise3/problem5/problem5.go
@@ -1,3 +1,31 @@
 package problem5
 
-type Person struct{}
+import (
+	"fmt"
+)
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+func (p *Person) compareAge(other *Person) string {
+	if p.Age > other.Age {
+		return fmt.Sprintf("%s is younger than me.", other.Name)
+	} else if p.Age < other.Age {
+		return fmt.Sprintf("%s is older than me.", other.Name)
+	} else {
+		return fmt.Sprintf("%s is the same age as me.", other.Name)
+	}
+}
+
+func main() {
+
+	p1 := &Person{"Samuel", 24}
+	p2 := &Person{"Joel", 36}
+	p3 := &Person{"Lily", 24}
+
+	fmt.Println(p1.compareAge(p2))
+	fmt.Println(p2.compareAge(p1))
+	fmt.Println(p1.compareAge(p3))
+}
