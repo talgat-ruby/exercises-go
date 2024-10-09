a9f427d06798623c955dacfc125aa6cd7883c4c7
ArayMankenov <ara.nanan@yandex.ru>
2024-10-09 23:37:32 +0500

    change problems

--- exercise2/problem12/problem12.go
+++ exercise2/problem12/problem12.go
@@ -1,3 +1,23 @@
 package problem11
 
-func keysAndValues() {}
+import "sort"
+
+func keysAndValues[K string | int, V comparable](mp map[K]V) ([]K, []V) {
+
+	keys := make([]K, 0, len(mp))
+	values := make([]V, 0, len(mp))
+
+	for k := range mp {
+		keys = append(keys, k)
+	}
+
+	sort.Slice(keys, func(i, j int) bool {
+		return keys[i] < keys[j]
+	})
+
+	for _, k := range keys {
+		values = append(values, mp[k])
+	}
+
+	return keys, values
+}
--- exercise2/problem3/problem3.go
+++ exercise2/problem3/problem3.go
@@ -1,38 +1,32 @@
 package main
 
-func diagonalize(n int, corner string) [][]int {
+type dir string
 
+const (
+	ul dir = "ul"
+	ur dir = "ur"
+	ll dir = "ll"
+	lr dir = "lr"
+)
+
+func diagonalize(n int, d dir) [][]int {
 	matrix := make([][]int, n)
 	for i := range matrix {
 		matrix[i] = make([]int, n)
 	}
-
-	switch corner {
-	case "ul":
-		for r := 0; r < n; r++ {
-			for c := 0; c < n; c++ {
-				matrix[r][c] = r + c
-			}
-		}
-	case "ur":
-		for r := 0; r < n; r++ {
-			for c := 0; c < n; c++ {
-				matrix[r][c] = (n - 1 + r) - c
-			}
-		}
-	case "ll":
-		for r := 0; r < n; r++ {
-			for c := 0; c < n; c++ {
-				matrix[r][c] = (n - 1 - r) + c
-			}
-		}
-	case "lr":
-		for r := 0; r < n; r++ {
-			for c := 0; c < n; c++ {
-				matrix[r][c] = (n - 1 - c) + (n - 1 - r)
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			switch d {
+			case ul:
+				matrix[i][j] = i + j
+			case ur:
+				matrix[i][j] = i + (n - 1 - j)
+			case ll:
+				matrix[i][j] = (n - 1 - i) + j
+			case lr:
+				matrix[i][j] = (n - 1 - i) + (n - 1 - j)
 			}
 		}
 	}
-
 	return matrix
 }
