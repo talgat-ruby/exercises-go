ce264e581b343812d0f01e48c7c4e4d868078757
UAssylbek <154306656+UAssylbek@users.noreply.github.com>
2024-10-17 18:14:41 +0500

    Algo

--- exercise4/bot/internal/model/board.go
+++ exercise4/bot/internal/model/board.go
@@ -52,6 +52,136 @@ func (b *Board) String() string {
 }
 
 func (b *Board) calculateNewPosition(token Token) int {
+	count := 0
+	for _, p := range b {
+		if p != TokenEmpty {
+			count++
+		}
+	}
+	if count == 1 {
+		return 4
+	}
+	nums := make(map[int]int)
+
+	for j := 0; j < len(b); j++ {
+		if b[j] == TokenEmpty {
+			if j == 0 {
+				if b[j+1] == b[j+2] && b[j+2] != TokenEmpty {
+					nums[j] = j+2
+				}
+				if b[j+3] == b[j+6] && b[j+6] != TokenEmpty {
+					nums[j] = j+6
+				}
+				if b[j+4] == b[j+8] && b[j+8] != TokenEmpty {
+					nums[j] = j+8
+				}
+			}
+			if j == 1 {
+				if b[j-1] == b[j+1] && b[j+1] != TokenEmpty {
+					nums[j] = j+1
+				}
+				if b[j+3] == b[j+6] && b[j+6] != TokenEmpty {
+					nums[j] = j+6
+				}
+			}
+			if j == 2 {
+				if b[j-1] == b[j-2] && b[j-2] != TokenEmpty {
+					nums[j] = j-2
+				}
+				if b[j+3] == b[j+6] && b[j+6] != TokenEmpty {
+					nums[j] = j+6
+				}
+				if b[j+2] == b[j+4] && b[j+4] != TokenEmpty {
+					nums[j] = j+4
+				}
+			}
+			if j == 3 {
+				if b[j+1] == b[j+2] && b[j+2] != TokenEmpty {
+					nums[j] = j+2
+				}
+				if b[j+3] == b[j-3] && b[j-3] != TokenEmpty {
+					nums[j] = j-3
+				}
+			}
+			if j == 4 {
+				if b[j-1] == b[j+1] && b[j+1] != TokenEmpty {
+					nums[j] = j+1
+				}
+				if b[j+3] == b[j-3] && b[j-3] != TokenEmpty {
+					nums[j] = j-3
+				}
+				if b[j-2] == b[j+2] && b[j+2] != TokenEmpty {
+					nums[j] = j+2
+				}
+				if b[j-4] == b[j+4] && b[j+4] != TokenEmpty {
+					nums[j] = j+4
+				}
+
+			}
+			if j == 5 {
+				if b[j-1] == b[j-2] && b[j-2] != TokenEmpty {
+					nums[j] = j-2
+				}
+				if b[j+3] == b[j-3] && b[j-3] != TokenEmpty {
+					nums[j] = j-3
+				}
+			}
+			if j == 6 {
+				if b[j+1] == b[j+2] && b[j+2] != TokenEmpty {
+					nums[j] = j+2
+				}
+				if b[j-3] == b[j-6] && b[j-6] != TokenEmpty {
+					nums[j] = j-6
+				}
+				if b[j-2] == b[j-4] && b[j-4] != TokenEmpty {
+					nums[j] = j-4
+				}
+			}
+			if j == 7 {
+				if b[j-1] == b[j+1] && b[j+1] != TokenEmpty {
+					nums[j] = j+1
+				}
+				if b[j-3] == b[j-6] && b[j-6] != TokenEmpty {
+					nums[j] = j-6
+
+				}
+			}
+			if j == 8 {
+				if b[j-1] == b[j-2] && b[j-2] != TokenEmpty {
+					nums[j] = j-2
+				}
+				if b[j-3] == b[j-6] && b[j-6] != TokenEmpty {
+					nums[j] = j-6
+				}
+				if b[j-4] == b[j-8] && b[j-8] != TokenEmpty {
+					nums[j] = j-8
+				}
+			}
+		}
+	}
+
+	if len(nums) > 0 {
+		for key, value := range nums {
+			if b[value] == token {
+				return key
+			}
+		}
+		for key1 := range nums {
+			return key1
+		}
+	}
+	if b[0] == TokenEmpty {
+		return 0
+	}
+	if b[2] == TokenEmpty {
+		return 2
+	}
+	if b[6] == TokenEmpty {
+		return 6
+	}
+	if b[8] == TokenEmpty {
+		return 8
+	}
 	for i, t := range b {
 		if t == TokenEmpty {
 			return i
