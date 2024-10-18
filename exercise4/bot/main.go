2c67699b30533ebe4d2a5101f3e0d1c8b0abe1ad
tobirama <g.kemalatdin@documentolog.com>
2024-10-18 18:53:51 +0500

    feat: send join request

--- exercise4/bot/main.go
+++ exercise4/bot/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"github.com/talgat-ruby/exercises-go/exercise4/bot/internal/client"
 	"os"
 	"os/signal"
 	"syscall"
@@ -13,7 +14,10 @@ func main() {
 	ready := startServer()
 	<-ready
 
-	// TODO after server start
+	name := os.Getenv("NAME")
+	port := os.Getenv("PORT")
+	url := "http://locolhost:" + port
+	client.SendJoinRequest(ctx, name, url)
 
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
