9585aa30014551981e2f1a454966b2269abba32e
UAssylbek <154306656+UAssylbek@users.noreply.github.com>
2024-10-18 17:20:08 +0500

    optimized algorithm

--- exercise4/bot/internal/model/board.go
+++ exercise4/bot/internal/model/board.go
@@ -15,6 +15,8 @@ const (
 	Rows = 3
 )
 
+var motion int
+
 type Board [Cols * Rows]Token
 
 func (b *Board) CalculateNewPosition(token Token) int {
@@ -52,140 +54,129 @@ func (b *Board) String() string {
 }
 
 func (b *Board) calculateNewPosition(token Token) int {
+	// Увеличиваем счётчик ходов
+	motion++
+	
+	// Проверяем количество заполненных ячеек на доске
 	count := 0
 	for _, p := range b {
 		if p != TokenEmpty {
 			count++
 		}
 	}
-	if count == 1 {
+	
+	// Если на доске только один ход и центральная ячейка пуста, ставим токен в центр
+	if count == 1 && b[4] == TokenEmpty {
 		return 4
 	}
-	nums := make(map[int]int)
 
-	for j := 0; j < len(b); j++ {
-		if b[j] == TokenEmpty {
-			if j == 0 {
-				if b[j+1] == b[j+2] && b[j+2] != TokenEmpty {
-					nums[j] = j+2
-				}
-				if b[j+3] == b[j+6] && b[j+6] != TokenEmpty {
-					nums[j] = j+6
-				}
-				if b[j+4] == b[j+8] && b[j+8] != TokenEmpty {
-					nums[j] = j+8
-				}
-			}
-			if j == 1 {
-				if b[j-1] == b[j+1] && b[j+1] != TokenEmpty {
-					nums[j] = j+1
-				}
-				if b[j+3] == b[j+6] && b[j+6] != TokenEmpty {
-					nums[j] = j+6
-				}
-			}
-			if j == 2 {
-				if b[j-1] == b[j-2] && b[j-2] != TokenEmpty {
-					nums[j] = j-2
-				}
-				if b[j+3] == b[j+6] && b[j+6] != TokenEmpty {
-					nums[j] = j+6
-				}
-				if b[j+2] == b[j+4] && b[j+4] != TokenEmpty {
-					nums[j] = j+4
-				}
-			}
-			if j == 3 {
-				if b[j+1] == b[j+2] && b[j+2] != TokenEmpty {
-					nums[j] = j+2
-				}
-				if b[j+3] == b[j-3] && b[j-3] != TokenEmpty {
-					nums[j] = j-3
-				}
-			}
-			if j == 4 {
-				if b[j-1] == b[j+1] && b[j+1] != TokenEmpty {
-					nums[j] = j+1
-				}
-				if b[j+3] == b[j-3] && b[j-3] != TokenEmpty {
-					nums[j] = j-3
-				}
-				if b[j-2] == b[j+2] && b[j+2] != TokenEmpty {
-					nums[j] = j+2
-				}
-				if b[j-4] == b[j+4] && b[j+4] != TokenEmpty {
-					nums[j] = j+4
-				}
+	// Получаем все возможные варианты выигрышных комбинаций
+	alloptions := b.allWin()             
+	// Находим оптимальные ходы для текущего игрока
+	optmoves := optimalMoves(alloptions) 
 
-			}
-			if j == 5 {
-				if b[j-1] == b[j-2] && b[j-2] != TokenEmpty {
-					nums[j] = j-2
-				}
-				if b[j+3] == b[j-3] && b[j-3] != TokenEmpty {
-					nums[j] = j-3
-				}
-			}
-			if j == 6 {
-				if b[j+1] == b[j+2] && b[j+2] != TokenEmpty {
-					nums[j] = j+2
-				}
-				if b[j-3] == b[j-6] && b[j-6] != TokenEmpty {
-					nums[j] = j-6
-				}
-				if b[j-2] == b[j-4] && b[j-4] != TokenEmpty {
-					nums[j] = j-4
-				}
-			}
-			if j == 7 {
-				if b[j-1] == b[j+1] && b[j+1] != TokenEmpty {
-					nums[j] = j+1
-				}
-				if b[j-3] == b[j-6] && b[j-6] != TokenEmpty {
-					nums[j] = j-6
+	// Проверяем, есть ли выигрышные или защитные ходы
+	if len(optmoves) > 0 {
+		// Если есть возможность выиграть, выбираем этот ход. 
+		// Если нет, выбираем ход для защиты.
+		emptyToken := findEmptySlotForToken(optmoves, token)
+		return emptyToken
+	}
 
-				}
+	// Если нет выигрышных или защитных ходов, ищем пустые углы
+	emtyCorner := b.findEmptyCorners()
+	return emtyCorner
+}
+
+func (b *Board) allWin() []map[int]Token {
+	// Определяем возможные выигрышные комбинации
+	winCombinations := [][]int{
+		{0, 1, 2}, // Горизонтальные линии
+		{3, 4, 5},
+		{6, 7, 8},
+		{0, 3, 6}, // Вертикальные линии
+		{1, 4, 7},
+		{2, 5, 8},
+		{0, 4, 8}, // Диагонали
+		{2, 4, 6},
+	}
+
+	// Инициализируем массив для хранения всех выигрышных комбинаций
+	alloptions := []map[int]Token{}
+
+	// Заполняем массив выигрышных комбинаций текущими значениями с доски
+	for _, combination := range winCombinations {
+		option := make(map[int]Token)
+		for _, index := range combination {
+			option[index] = b[index]
+		}
+		alloptions = append(alloptions, option)
+	}
+
+	return alloptions
+}
+
+func optimalMoves(alloptions []map[int]Token) []map[int]Token {
+	// Инициализируем массив для хранения оптимальных ходов
+	optmoves := []map[int]Token{}
+
+	// Перебираем все выигрышные комбинации
+	for _, option := range alloptions {
+		a := []Token{}
+		for _, value := range option {
+			a = append(a, value)
+		}
+		
+		// Проверяем условия для победы или защиты
+		if (a[0] == a[1] && a[0] != TokenEmpty && a[2] == TokenEmpty) ||
+			(a[0] == a[2] && a[0] != TokenEmpty && a[1] == TokenEmpty) ||
+			(a[1] == a[2] && a[1] != TokenEmpty && a[0] == TokenEmpty) {
+			// Добавляем текущую комбинацию в оптимальные ходы
+			optmoves = append(optmoves, option)
+		}
+	}
+	return optmoves
+}
+
+func findEmptySlotForToken(variants []map[int]Token, token Token) int {
+	var emptyIndex int
+	
+	// Перебираем все варианты
+	for _, variant := range variants {
+		hasToken := false
+
+		// Проверяем, есть ли токен и запоминаем индекс пустой ячейки
+		for key, value := range variant {
+			if value == token {
+				hasToken = true
+			} else if value == TokenEmpty {
+				emptyIndex = key
 			}
-			if j == 8 {
-				if b[j-1] == b[j-2] && b[j-2] != TokenEmpty {
-					nums[j] = j-2
-				}
-				if b[j-3] == b[j-6] && b[j-6] != TokenEmpty {
-					nums[j] = j-6
-				}
-				if b[j-4] == b[j-8] && b[j-8] != TokenEmpty {
-					nums[j] = j-8
-				}
-			}
+		}
+
+		// Если найден токен, возвращаем индекс пустой ячейки
+		if hasToken {
+			return emptyIndex
 		}
 	}
 
-	if len(nums) > 0 {
-		for key, value := range nums {
-			if b[value] == token {
-				return key
+	return emptyIndex // Возвращаем индекс пустой ячейки 
+}
+
+func (b *Board) findEmptyCorners() int {
+	// Определяем индексы угловых ячеек
+	corners := []int{0, 2, 6, 8} 
+
+	// Перебираем угловые ячейки
+	for _, index := range corners {
+		if b[index] == TokenEmpty {
+			// Пропускаем индекс 2, если Motion == 2
+			if index == 2 && motion == 2 {
+				continue
 			}
-		}
-		for key1 := range nums {
-			return key1
+			return index // Возвращаем индекс пустой угловой ячейки
 		}
 	}
-	if b[0] == TokenEmpty {
-		return 0
-	}
-	if b[2] == TokenEmpty {
-		return 2
-	}
-	if b[6] == TokenEmpty {
-		return 6
-	}
-	if b[8] == TokenEmpty {
-		return 8
-	}
-	for i, t := range b {
-		if t == TokenEmpty {
-			return i
-		}
-	}
-	return -1
+
+	return -1 // Возвращаем -1, если все угловые ячейки заняты
 }
