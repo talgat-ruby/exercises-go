8506459bf6476cdaffea502bbc6153eaedca8a42
aqzholsky <98736657+aqzholsky@users.noreply.github.com>
2024-10-29 17:31:17 +0500

    exercise 5 completed

--- exercise5/problem6/problem6.go
+++ exercise5/problem6/problem6.go
@@ -2,8 +2,33 @@ package problem6
 
 type pipe func(in <-chan int) <-chan int
 
-var multiplyBy2 pipe = func() {}
+var multiplyBy2 pipe = func(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for v := range in {
+			out <- v * 2
+		}
+		close(out)
+	}()
+	return out
+}
 
-var add5 pipe = func() {}
+var add5 pipe = func(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for v := range in {
+			out <- v + 5
+		}
+		close(out)
+	}()
+	return out
+}
 
-func piper(in <-chan int, pipes []pipe) <-chan int {}
+func piper(in <-chan int, pipes []pipe) <-chan int {
+	var out <-chan int
+	for _, pipe := range pipes {
+		out = pipe(in)
+		in = out
+	}
+	return out
+}
