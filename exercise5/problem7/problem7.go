5ee5cc17f31db08c79b057d479bcfc409f5a2270
Askar Imagnali <imangali950@gmail.com>
2024-11-04 00:46:36 +0500

    feature: added exercise5

--- exercise5/problem7/problem7.go
+++ exercise5/problem7/problem7.go
@@ -1,3 +1,25 @@
 package problem7
 
-func multiplex(ch1 <-chan string, ch2 <-chan string) []string {}
+func multiplex(ch1 <-chan string, ch2 <-chan string) []string {
+	output := make([]string, 0)
+
+	for {
+		v, more := <-ch1
+		if more {
+			output = append(output, v)
+		} else {
+			break
+		}
+	}
+
+	for {
+		v, more := <-ch2
+		if more {
+			output = append(output, v)
+		} else {
+			break
+		}
+	}
+
+	return output
+}
