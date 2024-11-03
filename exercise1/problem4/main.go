b31f2b6b7e5f36638058ac86f045a635ba099d7d
akabdylk <kabdylkakarnur@gmail.com>
2024-11-04 01:36:34 +0500

    exercise5

--- exercise1/problem4/main.go
+++ exercise1/problem4/main.go
@@ -1,3 +1,15 @@
 package main
 
-func detectWord() {}
+import (
+	"unicode"
+)
+
+func detectWord(crowd string) string {
+	var result string
+	for _, char := range crowd {
+		if unicode.IsLower(char) {
+			result += string(char)
+		}
+	}
+	return result
+}
