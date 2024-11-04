8e2363f160bea771547b7310cb15620f5840cd31
zhandaulet19 <zhandaulet.zhakupov@gmail.com>
2024-11-04 23:26:26 +0500

    exercises5

--- exercise5/problem2/problem2.go
+++ exercise5/problem2/problem2.go
@@ -1,6 +1,10 @@
 package problem2
 
-// add - sequential code to add numbers, don't update it, just to illustrate concept
+import (
+	"runtime"
+	"sync"
+)
+
 func add(numbers []int) int64 {
 	var sum int64
 	for _, n := range numbers {
@@ -11,6 +15,35 @@ func add(numbers []int) int64 {
 
 func addConcurrently(numbers []int) int64 {
 	var sum int64
+	numCPU := runtime.NumCPU()
+	chunkSize := len(numbers) / numCPU
 
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+
+	for i := 0; i < numCPU; i++ {
+		wg.Add(1)
+
+		start := i * chunkSize
+		end := start + chunkSize
+		if i == numCPU-1 {
+			end = len(numbers)
+		}
+
+		go func(start, end int) {
+			defer wg.Done()
+
+			var partialSum int64
+			for _, n := range numbers[start:end] {
+				partialSum += int64(n)
+			}
+
+			mu.Lock()
+			sum += partialSum
+			mu.Unlock()
+		}(start, end)
+	}
+
+	wg.Wait()
 	return sum
 }
--- exercise5/problem5/problem5.go
+++ exercise5/problem5/problem5.go
@@ -1,17 +1,25 @@
 package problem5
 
-func producer() {}
-
-func consumer() {}
-
-func send(
-	words []string,
-	pr func([]string, chan<- string),
-	cons func(<-chan string) string,
-) string {
+func send(words []string, pr func([]string, chan<- string), cons func(<-chan string) string) string {
 	ch := make(chan string)
-
 	go pr(words, ch)
-
 	return cons(ch)
+}
+
+func producer(words []string, ch chan<- string) {
+	for _, word := range words {
+		ch <- word
+	}
+	close(ch)
+}
+
+func consumer(ch <-chan string) string {
+	var result string
+	for word := range ch {
+		if len(result) > 0 {
+			result += " "
+		}
+		result += word
+	}
+	return result
 }
