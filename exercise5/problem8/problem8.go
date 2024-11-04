49333e789de603583a8330251b625273ae4f859b
Adlet <adlet.zh19966@gmail.com>
2024-11-04 16:55:02 +0600

    feature/exercise5

--- exercise5/problem8/problem8.go
+++ exercise5/problem8/problem8.go
@@ -4,4 +4,16 @@ import (
 	"time"
 )
 
-func withTimeout(ch <-chan string, ttl time.Duration) string {}
+func withTimeout(ch <-chan string, ttl time.Duration) string {
+	// Создаем таймер с заданным временем жизни
+	timer := time.NewTimer(ttl)
+
+	select {
+	case msg := <-ch:
+		// Сообщение получено до истечения времени
+		return msg
+	case <-timer.C:
+		// Таймер сработал, время истекло
+		return "timeout"
+	}
+}
