19ff4083fb106696b3bdd6e731dbef4c13909e34
Adlet <adlet.zh19966@gmail.com>
2024-11-04 16:55:02 +0600

    feature/exercise5

--- exercise5/problem2/problem2.go
+++ exercise5/problem2/problem2.go
@@ -1,5 +1,7 @@
 package problem2
 
+import "sync"
+
 // add - sequential code to add numbers, don't update it, just to illustrate concept
 func add(numbers []int) int64 {
 	var sum int64
@@ -9,8 +11,31 @@ func add(numbers []int) int64 {
 	return sum
 }
 
+// Функция addConcurrently — параллельное сложение чисел
 func addConcurrently(numbers []int) int64 {
 	var sum int64
+	var wg sync.WaitGroup
+	sumChan := make(chan int64) // Небуферизированный канал
+
+	// Запускаем горутину для каждого числа
+	for _, num := range numbers {
+		wg.Add(1)
+		go func(n int) {
+			defer wg.Done()
+			sumChan <- int64(n) // Отправляем число в канал
+		}(num)
+	}
+
+	// Горутина для чтения из канала и суммирования
+	go func() {
+		wg.Wait()      // Ждем завершения всех горутин
+		close(sumChan) // Закрываем канал после завершения всех горутин
+	}()
+
+	// Читаем из канала и суммируем
+	for partialSum := range sumChan {
+		sum += partialSum
+	}
 
 	return sum
 }
