69c49317ae63b7a39ab5cb2170ee061454fd75e4
galyym <ksgalym@mail.ru>
2024-11-07 02:13:04 +0500

    feat: exercise5

--- exercise5/problem3/problem3.go
+++ exercise5/problem3/problem3.go
@@ -2,10 +2,11 @@ package problem3
 
 func sum(a, b int) int {
 	var c int
-
+	ch := make(chan int)
 	go func(a, b int) {
 		c = a + b
+		ch <- c
 	}(a, b)
-
+	c = <-ch
 	return c
 }
--- exercise5/problem4/problem4.go
+++ exercise5/problem4/problem4.go
@@ -1,16 +1,25 @@
 package problem4
 
-func iter(ch chan<- int, nums []int) {
+import "sync"
+
+func iter(ch chan<- int, nums []int, wg *sync.WaitGroup) {
 	for _, n := range nums {
 		ch <- n
 	}
+	wg.Done()
 }
 
 func sum(nums []int) int {
 	ch := make(chan int)
+	var wg sync.WaitGroup
 
-	go iter(ch, nums)
+	wg.Add(1)
+	go iter(ch, nums, &wg)
 
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
 	var sum int
 	for n := range ch {
 		sum += n
