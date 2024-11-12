07272c978e0329575946f6258e6226c5f18d19d7
EldarActorGuy <eldar.imsc@gmail.com>
2024-11-12 23:52:25 +0500

    ELdar exercise1 problem4

--- exercise1/problem5/main.go
+++ exercise1/problem5/main.go
@@ -1,12 +1,15 @@
 package main
 
-func potatoes(crowd string) int {
-	var result int
-	strL := len("potato")
-	for i := 0; i <= len(crowd)-strL; i++ {
-		if crowd[i:i+strL] == "potato" {
-			result++
-		}
-	}
-	return result
+import (
+    "strings"
+)
+
+func detectWord(crowd string) string {
+    var word strings.Builder
+    for _, ch := range crowd {
+        if ch >= 'a' && ch <= 'z' {
+            word.WriteRune(ch)
+        }
+    }
+    return word.String()
 }
