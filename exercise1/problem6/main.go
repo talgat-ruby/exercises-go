0485d67f33139fa6d2f77951af1dcf12ffbeda9a
EldarActorGuy <eldar.imsc@gmail.com>
2024-11-12 23:54:14 +0500

    Eldar exercise1 problem5

--- exercise1/problem6/main.go
+++ exercise1/problem6/main.go
@@ -2,18 +2,6 @@ package main
 
 import "strings"
 
-var emoji = map[string]string{
-	"smile": "🙂",
-	"grin":  "😀",
-	"sad":   "😥",
-	"mad":   "😠",
-}
-
-func emojify(text string) string {
-	for i, v := range emoji {
-		if strings.Contains(text, i) {
-			text = strings.Replace(text, i, v, 1)
-		}
-	}
-	return text
+func potatoes(str string) int {
+    return strings.Count(str, "potato")
 }
