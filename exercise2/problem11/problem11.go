d6a24b9c4a53cafbef2d2574b90226bb9a33a86f
EldarActorGuy <42513949+EldarActorGuy@users.noreply.github.com>
2024-11-13 09:56:55 +0500

    Ex2_prob11 Eldar Azb

--- exercise2/problem11/problem11.go
+++ exercise2/problem11/problem11.go
@@ -8,7 +8,7 @@ func removeDups[T comparable](items []T) []T {
 	result := []T{}
 
 	for _, item := range items {
-		// If item is not in the map, add it to the result and mark it as seen
+		// If the item hasn't been seen before, add it to the result slice
 		if !seen[item] {
 			seen[item] = true
 			result = append(result, item)
@@ -19,7 +19,7 @@ func removeDups[T comparable](items []T) []T {
 }
 
 func main() {
-	fmt.Println(removeDups([]int{1, 0, 1, 0}))             // Output: [1, 0]
+	fmt.Println(removeDups([]int{1, 0, 1, 0}))                // Output: [1, 0]
 	fmt.Println(removeDups([]bool{true, false, false, true})) // Output: [true, false]
 	fmt.Println(removeDups([]string{"John", "Taylor", "John"})) // Output: ["John", "Taylor"]
 }
