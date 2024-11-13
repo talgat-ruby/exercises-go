fe9ed02f599f8e48964ea9070e5db76055929f0a
EldarActorGuy <42513949+EldarActorGuy@users.noreply.github.com>
2024-11-13 10:38:03 +0500

    ex5 prob 5 eldar

--- exercise5/problem5/problem5.go
+++ exercise5/problem5/problem5.go
@@ -1,31 +1,47 @@
-package problem5
+package main
 
-func producer(list []string, ch chan<- string) {
-	for _, v := range list {
-		ch <- v
+import (
+	"fmt"
+	"strings"
+)
+
+// producer sends each word in the list to the channel
+func producer(words []string, ch chan<- string) {
+	for _, word := range words {
+		ch <- word // send each word to the channel
 	}
-	close(ch)
+	close(ch) // close the channel when done
 }
 
+// consumer reads words from the channel and combines them into a single message
 func consumer(ch <-chan string) string {
-	var text string
-	for v := range ch {
-		if text != "" {
-			text += " "
-		}
-		text = text + v
+	var message []string
+	for word := range ch {
+		message = append(message, word) // collect words from the channel
 	}
-	return text
+	return strings.Join(message, " ") // join the words with a space
 }
 
-func send(
-	words []string,
-	pr func([]string, chan<- string),
-	cons func(<-chan string) string,
-) string {
+// send combines the functionality of producer and consumer to create the final message
+func send(words []string, producerFunc func([]string, chan<- string), consumerFunc func(<-chan string) string) string {
 	ch := make(chan string)
+	go producerFunc(words, ch)       // start the producer goroutine
+	return consumerFunc(ch)          // consume from the channel and return the combined message
+}
 
-	go pr(words, ch)
+func main() {
+	list := []string{
+		"Hello",
+		"dear",
+		"friend!",
+		"Learn",
+		"from",
+		"yesterday.",
+		"Save",
+		"our",
+		"soles.",
+	}
 
-	return cons(ch)
+	message := send(list, producer, consumer)
+	fmt.Println(message) // Output: "Hello dear friend! Learn from yesterday. Save our soles."
 }
