fcd5360ba267d9cdf64281a01125338ee008bcfc
EldarActorGuy <42513949+EldarActorGuy@users.noreply.github.com>
2024-11-13 09:49:03 +0500

    Ex2_prob4 Eldar Azb

--- exercise2/problem4/problem4.go
+++ exercise2/problem4/problem4.go
@@ -1,11 +1,25 @@
-package problem4
+package main
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
-func mapping(inp []string) map[string]string {
-	intM := make(map[string]string)
-	for _, v := range inp {
-		intM[v] = strings.ToUpper(v)
+// mapping takes a slice of lowercase letters and returns a map where each key is lowercase
+// and the corresponding value is the uppercase version of that letter
+func mapping(letters []string) map[string]string {
+	letterMap := make(map[string]string)
+
+	for _, letter := range letters {
+		// Convert the letter to uppercase and add it to the map
+		letterMap[letter] = strings.ToUpper(letter)
 	}
-	return intM
+
+	return letterMap
+}
+
+func main() {
+	fmt.Println(mapping([]string{"p", "s"}))           // Output: { "p": "P", "s": "S" }
+	fmt.Println(mapping([]string{"a", "b", "c"}))       // Output: { "a": "A", "b": "B", "c": "C" }
+	fmt.Println(mapping([]string{"a", "v", "y", "z"}))  // Output: { "a": "A", "v": "V", "y": "Y", "z": "Z" }
 }
