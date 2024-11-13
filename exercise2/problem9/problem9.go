fb3ea4452916d915534c38334ec96bb69dcdb859
EldarActorGuy <42513949+EldarActorGuy@users.noreply.github.com>
2024-11-13 09:55:02 +0500

    Ex2_prob9 Eldar Azb

--- exercise2/problem9/problem9.go
+++ exercise2/problem9/problem9.go
@@ -1,11 +1,23 @@
-package problem9
+package main
 
-func factory(multiple int) func(nums ...int) []int {
+import "fmt"
+
+// factory takes an integer multiplier and returns a function that multiplies each of its arguments by that multiplier
+func factory(multiplier int) func(...int) []int {
+	// Return a function that takes a variable number of ints and returns a slice of ints
 	return func(nums ...int) []int {
-		result := make([]int, 0)
-		for _, v := range nums {
-			result = append(result, v*multiple)
+		result := make([]int, len(nums))
+		for i, num := range nums {
+			result[i] = num * multiplier
 		}
 		return result
 	}
+}
+
+func main() {
+	first := factory(15)
+	fmt.Println(first(2, 3, 4)) // Output: [30, 45, 60]
+
+	second := factory(2)
+	fmt.Println(second(1, 2, 3, 4)) // Output: [2, 4, 6, 8]
 }
