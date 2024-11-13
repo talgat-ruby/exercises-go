34d5ef98bf1f7f160f8241edaf3a040cf06b8df2
EldarActorGuy <42513949+EldarActorGuy@users.noreply.github.com>
2024-11-13 10:27:17 +0500

    Ex3_prob5 Eldar

--- exercise3/problem5/problem5.go
+++ exercise3/problem5/problem5.go
@@ -1,15 +1,29 @@
-package problem5
+package main
 
+import "fmt"
+
+// Person represents a person with a name and age
 type Person struct {
 	name string
 	age  int
 }
 
-func (m *Person) compareAge(person *Person) string {
-	if m.age < person.age {
-		return person.name + " is older than me."
-	} else if m.age > person.age {
-		return person.name + " is younger than me."
+// compareAge compares the age of another person with the receiver's age and returns a formatted string
+func (p *Person) compareAge(other *Person) string {
+	if p.age < other.age {
+		return fmt.Sprintf("%s is older than me.", other.name)
+	} else if p.age > other.age {
+		return fmt.Sprintf("%s is younger than me.", other.name)
 	}
-	return person.name + " is the same age as me."
+	return fmt.Sprintf("%s is the same age as me.", other.name)
+}
+
+func main() {
+	p1 := &Person{"Samuel", 24}
+	p2 := &Person{"Joel", 36}
+	p3 := &Person{"Lily", 24}
+
+	fmt.Println(p1.compareAge(p2)) // Output: "Joel is older than me."
+	fmt.Println(p2.compareAge(p1)) // Output: "Samuel is younger than me."
+	fmt.Println(p1.compareAge(p3)) // Output: "Lily is the same age as me."
 }
