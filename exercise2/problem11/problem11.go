775bb5e0845078b85a42c5e7c53aaa4a4099b7e8
EldarActorGuy <42513949+EldarActorGuy@users.noreply.github.com>
2024-11-13 09:56:16 +0500

    Ex2_Prob11 Eldar Azb

--- exercise2/problem11/problem11.go
+++ exercise2/problem11/problem11.go
@@ -1,13 +1,25 @@
-package problem11
+package main
 
-import "slices"
+import "fmt"
 
-func removeDups[T comparable](nums []T) []T {
-	result := make([]T, 0)
-	for _, v := range nums {
-		if !slices.Contains(result, v) {
-			result = append(result, v)
+// removeDups removes duplicate items from a slice while maintaining order
+func removeDups[T comparable](items []T) []T {
+	seen := make(map[T]bool)
+	result := []T{}
+
+	for _, item := range items {
+		// If item is not in the map, add it to the result and mark it as seen
+		if !seen[item] {
+			seen[item] = true
+			result = append(result, item)
 		}
 	}
+
 	return result
+}
+
+func main() {
+	fmt.Println(removeDups([]int{1, 0, 1, 0}))             // Output: [1, 0]
+	fmt.Println(removeDups([]bool{true, false, false, true})) // Output: [true, false]
+	fmt.Println(removeDups([]string{"John", "Taylor", "John"})) // Output: ["John", "Taylor"]
 }
