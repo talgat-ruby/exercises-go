c4b6dda09840e9265e2c1f3f11910a23f0cc507b
EldarActorGuy <42513949+EldarActorGuy@users.noreply.github.com>
2024-11-13 10:39:42 +0500

    ex5 prob8 eldar

--- exercise5/problem8/problem8.go
+++ exercise5/problem8/problem8.go
@@ -1,16 +1,49 @@
-package problem8
+package main
 
 import (
+	"fmt"
 	"time"
 )
 
-func withTimeout(ch <-chan string, ttl time.Duration) string {
-	for {
+// withTimeout waits for a message on the input channel until the specified ttl (time-to-live).
+// If a message is received before the timeout, it forwards the message to the output channel.
+// Otherwise, it sends "timeout" to the output channel after the ttl duration.
+func withTimeout(input <-chan string, ttl time.Duration) <-chan string {
+	output := make(chan string)
+
+	go func() {
+		defer close(output)
 		select {
-		case v := <-ch:
-			return v
+		case msg := <-input:
+			// If a message is received from input before the timeout
+			output <- msg
 		case <-time.After(ttl):
-			return "timeout"
+			// If the timeout duration passes before receiving a message
+			output <- "timeout"
 		}
-	}
+	}()
+
+	return output
+}
+
+func main() {
+	input := make(chan string)
+
+	// Test with message received before timeout
+	go func() {
+		time.Sleep(1 * time.Second)
+		input <- "hello"
+	}()
+
+	output := withTimeout(input, 2*time.Second)
+	fmt.Println(<-output) // Should print "hello" since it arrives before the 2-second ttl
+
+	// Test with message arriving after timeout
+	go func() {
+		time.Sleep(3 * time.Second)
+		input <- "world"
+	}()
+
+	output = withTimeout(input, 2*time.Second)
+	fmt.Println(<-output) // Should print "timeout" since 2-second ttl expires before "world" arrives
 }
