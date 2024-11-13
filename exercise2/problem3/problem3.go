b2caccf1da28d0eef145dfeb4115ab25ee8b98f5
EldarActorGuy <42513949+EldarActorGuy@users.noreply.github.com>
2024-11-13 09:48:26 +0500

    Ex2_prob3 Eldar Azb

--- exercise2/problem3/problem3.go
+++ exercise2/problem3/problem3.go
@@ -1,51 +1,39 @@
 package main
 
-type dir string
-
-const (
-	ul dir = "ul"
-	ur dir = "ur"
-	ll dir = "ll"
-	lr dir = "lr"
+import (
+	"fmt"
 )
 
-func diagonalize(n int, d dir) [][]int {
+// diagonalize creates an n x n matrix with numbers ordered diagonally based on the specified corner
+func diagonalize(n int, corner string) [][]int {
+	// Initialize an empty n x n matrix
 	matrix := make([][]int, n)
-	switch d {
-	case ul:
-		count := 0
-		getMatrix(count, matrix, n, true, true)
-		break
-	case ll:
-		count := n - 1
-		getMatrix(count, matrix, n, true, false)
-		break
-	case ur:
-		count := n - 1
-		getMatrix(count, matrix, n, false, true)
-		break
-	case lr:
-		count := (n - 1) * 2
-		getMatrix(count, matrix, n, false, false)
-		break
+	for i := range matrix {
+		matrix[i] = make([]int, n)
 	}
+
+	// Fill the matrix based on the specified corner
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			switch corner {
+			case "ul": // Upper-left corner
+				matrix[i][j] = i + j
+			case "ur": // Upper-right corner
+				matrix[i][j] = (n - 1 - i) + j
+			case "ll": // Lower-left corner
+				matrix[i][j] = i + (n - 1 - j)
+			case "lr": // Lower-right corner
+				matrix[i][j] = (n - 1 - i) + (n - 1 - j)
+			}
+		}
+	}
+
 	return matrix
 }
 
-func getMatrix(count int, matrix [][]int, n int, o bool, countChecker bool) {
-	for i := range n {
-		matrix[i] = make([]int, n)
-		for j := range n {
-			if o {
-				matrix[i][j] = count + j
-			} else {
-				matrix[i][j] = count - j
-			}
-		}
-		if countChecker {
-			count++
-		} else {
-			count--
-		}
-	}
+func main() {
+	fmt.Println(diagonalize(3, "ul")) // [[0, 1, 2], [1, 2, 3], [2, 3, 4]]
+	fmt.Println(diagonalize(4, "ur")) // [[3, 2, 1, 0], [4, 3, 2, 1], [5, 4, 3, 2], [6, 5, 4, 3]]
+	fmt.Println(diagonalize(3, "ll")) // [[2, 3, 4], [1, 2, 3], [0, 1, 2]]
+	fmt.Println(diagonalize(5, "lr")) // [[8, 7, 6, 5, 4], [7, 6, 5, 4, 3], [6, 5, 4, 3, 2], [5, 4, 3, 2, 1], [4, 3, 2, 1, 0]]
 }
