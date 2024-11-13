2cb0f6f59a90cbff114c71d8e1d9fa184b965112
EldarActorGuy <42513949+EldarActorGuy@users.noreply.github.com>
2024-11-13 09:54:16 +0500

    Ex2_prob8 Eldar Azb

--- exercise2/problem8/problem8.go
+++ exercise2/problem8/problem8.go
@@ -1,15 +1,25 @@
-package problem8
+package main
 
-func simplify(list []string) map[string]int {
-	var indMap map[string]int
+import "fmt"
 
-	indMap = make(map[string]int)
-	load(indMap, list)
-	return indMap
+// simplify maps each element of the input slice to its index and returns the map
+func simplify(elements []string) map[string]int {
+	result := make(map[string]int)
+
+	// load function that stores the index of each element in the map
+	load := func(key string, index int) {
+		result[key] = index
+	}
+
+	// Iterate over elements and load them into the map
+	for i, elem := range elements {
+		load(elem, i)
+	}
+
+	return result
 }
 
-func load(m map[string]int, students []string) {
-	for i, name := range students {
-		m[name] = i
-	}
+func main() {
+	fmt.Println(simplify([]string{"a", "b", "c"}))               // Output: map[a:0 b:1 c:2]
+	fmt.Println(simplify([]string{"z", "y", "x", "u", "v"}))     // Output: map[u:3 v:4 x:2 y:1 z:0]
 }
