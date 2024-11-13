4f9793afcc22888a9858dfdf039422f55ee543e3
EldarActorGuy <42513949+EldarActorGuy@users.noreply.github.com>
2024-11-13 09:50:27 +0500

    Ex2_prob6 Eldar Azb

--- exercise2/problem6/problem6.go
+++ exercise2/problem6/problem6.go
@@ -1,12 +1,30 @@
-package problem6
+package main
 
-func sumOfTwo(first []int, second []int, sum int) bool {
-	for _, fv := range first {
-		for _, sv := range second {
-			if fv+sv == sum {
-				return true
-			}
+import "fmt"
+
+// sumOfTwo checks if there exists a pair of numbers from slices `a` and `b` that adds up to `v`
+func sumOfTwo(a, b []int, v int) bool {
+	// Create a map to store the required complement values from array `a`
+	complements := make(map[int]bool)
+
+	// Fill the map with the complements for each element in `a`
+	for _, num := range a {
+		complements[v - num] = true
+	}
+
+	// Check if any element in `b` exists in the map of complements
+	for _, num := range b {
+		if complements[num] {
+			return true
 		}
 	}
+
 	return false
+}
+
+func main() {
+	fmt.Println(sumOfTwo([]int{1, 2}, []int{4, 5, 6}, 5)) // Output: true
+	fmt.Println(sumOfTwo([]int{1, 2}, []int{4, 5, 6}, 8)) // Output: true
+	fmt.Println(sumOfTwo([]int{1, 2}, []int{4, 5, 6}, 3)) // Output: false
+	fmt.Println(sumOfTwo([]int{1, 2}, []int{4, 5, 6}, 9)) // Output: false
 }
