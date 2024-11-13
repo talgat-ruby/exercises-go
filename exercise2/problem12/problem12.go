bedfd1227e508963e0cd9b43b1d0758d55c88ac6
EldarActorGuy <42513949+EldarActorGuy@users.noreply.github.com>
2024-11-13 09:59:01 +0500

    Ex2_prob12 Eldar Azb

--- exercise2/problem12/problem12.go
+++ exercise2/problem12/problem12.go
@@ -1,32 +1,28 @@
-package problem11
+package main
 
 import (
 	"fmt"
 	"sort"
 )
 
+// keysAndValues takes a map and returns two slices: sorted keys and their corresponding values.
 func keysAndValues[K comparable, V any](m map[K]V) ([]K, []V) {
-	if m == nil {
-		return []K{}, []V{}
-	}
-
+	// Collect keys from the map
 	keys := make([]K, 0, len(m))
 	for k := range m {
 		keys = append(keys, k)
 	}
 
+	// Sort keys
 	sort.Slice(keys, func(i, j int) bool {
-		return stringify(keys[i]) < stringify(keys[j])
+		switch k := any(keys[i]).(type) {
+		case string:
+			return k < keys[j].(string)
+		case int:
+			return k < keys[j].(int)
+		default:
+			return fmt.Sprintf("%v", keys[i]) < fmt.Sprintf("%v", keys[j])
+		}
 	})
 
-	values := make([]V, len(keys))
-	for i, k := range keys {
-		values[i] = m[k]
-	}
-
-	return keys, values
-}
-
-func stringify[T any](v T) string {
-	return fmt.Sprintf("%v", v)
-}
+	// Collect values in the
