f2a17629ecfadd80a34e0512517efe22b9421b3a
EldarActorGuy <42513949+EldarActorGuy@users.noreply.github.com>
2024-11-13 10:27:51 +0500

    Ex3_prob6 Eldar

--- exercise3/problem6/problem6.go
+++ exercise3/problem6/problem6.go
@@ -1,31 +1,48 @@
-package problem6
+package main
 
-type Len interface {
-	Lens() int
+import "fmt"
+
+// Legged represents any entity with legs that can return a leg count
+type Legged interface {
+	GetLegsNum() int
 }
 
+// Animal represents an animal with a name and a number of legs
 type Animal struct {
 	name    string
 	legsNum int
 }
 
-func (a *Animal) Lens() int {
+// GetLegsNum returns the number of legs for an Animal
+func (a *Animal) GetLegsNum() int {
 	return a.legsNum
 }
 
+// Insect represents an insect with a name and a number of legs
 type Insect struct {
 	name    string
 	legsNum int
 }
 
-func (i *Insect) Lens() int {
+// GetLegsNum returns the number of legs for an Insect
+func (i *Insect) GetLegsNum() int {
 	return i.legsNum
 }
 
-func sumOfAllLegsNum(lens ...Len) int {
-	sum := 0
-	for _, v := range lens {
-		sum = sum + v.Lens()
+// sumOfAllLegsNum calculates the total number of legs for all provided Legged entities
+func sumOfAllLegsNum(leggeds ...Legged) int {
+	totalLegs := 0
+	for _, legged := range leggeds {
+		totalLegs += legged.GetLegsNum()
 	}
-	return sum
+	return totalLegs
+}
+
+func main() {
+	horse := &Animal{name: "horse", legsNum: 4}
+	kangaroo := &Animal{name: "kangaroo", legsNum: 2}
+	ant := &Insect{name: "ant", legsNum: 6}
+	spider := &Insect{name: "spider", legsNum: 8}
+
+	fmt.Println(sumOfAllLegsNum(horse, kangaroo, ant, spider)) // Output: 20
 }
