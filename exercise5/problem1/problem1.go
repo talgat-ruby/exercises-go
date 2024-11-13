c7344f8dad24f4b16841dedac868a5b75c894a06
EldarActorGuy <42513949+EldarActorGuy@users.noreply.github.com>
2024-11-13 10:35:27 +0500

    ex5 prob1 eldar

--- exercise5/problem1/problem1.go
+++ exercise5/problem1/problem1.go
@@ -1,12 +1,33 @@
-package problem1
+package main
 
-func incrementConcurrently(num int) int {
-	ch := make(chan int)
-	defer close(ch)
-	go func() {
-		num++
-		ch <- 1
-	}()
-	<-ch
-	return num
+import (
+	"fmt"
+	"sync"
+)
+
+type Counter struct {
+	value int
+	mu    sync.Mutex
 }
+
+func (c *Counter) Increment() {
+	c.mu.Lock()
+	c.value++
+	c.mu.Unlock()
+}
+
+func (c *Counter) Value() int {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.value
+}
+
+// incrementConcurrently increments the counter concurrently across n goroutines
+func incrementConcurrently(counter *Counter, n int) {
+	var wg sync.WaitGroup
+	wg.Add(n)
+	for i := 0; i < n; i++ {
+		go func() {
+			defer wg.Done()
+			counter.Increment()
+		}(
