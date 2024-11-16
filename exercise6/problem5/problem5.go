fbdbfd83c86998fac15a77757c07534a4b8f24f4
Adlet <adlet.zh19966@gmail.com>
2024-11-16 15:28:28 +0600

    exercise6

--- exercise6/problem5/problem5.go
+++ exercise6/problem5/problem5.go
@@ -1,31 +1,54 @@
 package problem5
 
 import (
+	"sync"
 	"time"
 )
 
-func worker(id int, shoppingList *[]string, ch chan<- int) {
-	// TODO wait for shopping list to be completed
-	ch <- id
+// Worker function
+func worker(id int, shoppingList *[]string, wg *sync.WaitGroup, mutex *sync.Mutex, ch chan<- int) {
+	defer wg.Done()
+
+	for {
+		mutex.Lock()
+		// Check if the shopping list is filled
+		if len(*shoppingList) > 0 {
+			ch <- id
+			mutex.Unlock()
+			return
+		}
+		mutex.Unlock()
+		time.Sleep(time.Millisecond) // Allow other workers to check
+	}
 }
 
-func updateShopList(shoppingList *[]string) {
+// Updates the shopping list
+func updateShopList(shoppingList *[]string, mutex *sync.Mutex) {
 	time.Sleep(10 * time.Millisecond)
 
-	*shoppingList = append(*shoppingList, "apples")
-	*shoppingList = append(*shoppingList, "milk")
-	*shoppingList = append(*shoppingList, "bake soda")
+	mutex.Lock()
+	*shoppingList = append(*shoppingList, "apples", "milk", "bake soda")
+	mutex.Unlock()
 }
 
+// Notify workers once the shopping list is filled
 func notifyOnShopListUpdate(shoppingList *[]string, numWorkers int) <-chan int {
 	notifier := make(chan int)
+	var wg sync.WaitGroup
+	var mutex sync.Mutex
 
-	for i := range numWorkers {
-		go worker(i+1, shoppingList, notifier)
-		time.Sleep(time.Millisecond) // order matters
+	// Launch all workers
+	for i := 0; i < numWorkers; i++ {
+		wg.Add(1)
+		go worker(i+1, shoppingList, &wg, &mutex, notifier)
 	}
 
-	go updateShopList(shoppingList)
+	// Fill the shopping list
+	go func() {
+		updateShopList(shoppingList, &mutex)
+		wg.Wait() // Wait for all workers to finish
+		close(notifier)
+	}()
 
 	return notifier
 }
