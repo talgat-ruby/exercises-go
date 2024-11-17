8b19b5292ebdb13207c5816c8fd1ecd4b341a3dd
UAssylbek <154306656+UAssylbek@users.noreply.github.com>
2024-11-17 22:06:35 +0500

    done

--- exercise6/problem1/problem1.go
+++ exercise6/problem1/problem1.go
@@ -1,9 +1,34 @@
 package problem1
 
+import "sync"
+
 type bankAccount struct {
+	sync.Mutex
 	blnc int
 }
 
 func newAccount(blnc int) *bankAccount {
-	return &bankAccount{blnc}
+	return &bankAccount{blnc:blnc}
+}
+
+func (b *bankAccount) deposit(cash int) {
+	b.Mutex.Lock()
+	defer b.Mutex.Unlock()
+	b.blnc += cash
+}
+
+func (b *bankAccount) withdraw(cash int) bool {
+	b.Mutex.Lock()
+	defer b.Mutex.Unlock()
+	if b.blnc < cash {
+		return false
+	}
+	b.blnc -= cash
+	return true
+}
+
+func (b *bankAccount) getBalance() int {
+	b.Mutex.Lock()
+	defer b.Mutex.Unlock()
+	return b.blnc
 }
