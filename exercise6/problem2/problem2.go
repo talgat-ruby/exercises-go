e4bdcd109fbbbf3ccf4907de92a6508218407b32
araailym <araailymmm@gmail.com>
2024-11-18 19:27:12 +0500

    add solutions for exercise-6

--- exercise6/problem2/problem2.go
+++ exercise6/problem2/problem2.go
@@ -1,20 +1,41 @@
 package problem2
 
 import (
+	"sync"
 	"time"
 )
 
 var readDelay = 10 * time.Millisecond
 
 type bankAccount struct {
+	mu   sync.Mutex
 	blnc int
 }
 
 func newAccount(blnc int) *bankAccount {
-	return &bankAccount{blnc}
+	return &bankAccount{blnc: blnc}
 }
 
 func (b *bankAccount) balance() int {
 	time.Sleep(readDelay)
-	return 0
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	return b.blnc
+}
+
+func (b *bankAccount) deposit(amount int) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	b.blnc += amount
+}
+
+func (b *bankAccount) withdraw(amount int) bool {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	if b.blnc >= amount {
+		b.blnc -= amount
+		return true
+	}
+	return false
 }
