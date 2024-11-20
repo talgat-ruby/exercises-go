31d0ea3498f34f1fb487d91e5dcb6c1840776c19
aqzholsky <98736657+aqzholsky@users.noreply.github.com>
2024-11-20 22:35:32 +0500

    WIP

--- exercise6/problem1/problem1.go
+++ exercise6/problem1/problem1.go
@@ -1,9 +1,26 @@
 package problem1
 
+import "sync"
+
 type bankAccount struct {
 	blnc int
+	mx   *sync.Mutex
 }
 
 func newAccount(blnc int) *bankAccount {
-	return &bankAccount{blnc}
+	return &bankAccount{blnc, &sync.Mutex{}}
+}
+
+func (b *bankAccount) withdraw(amount int) {
+	defer b.mx.Unlock()
+	b.mx.Lock()
+	if b.blnc >= amount {
+		b.blnc -= amount
+	}
+}
+
+func (b *bankAccount) deposit(amount int) {
+	defer b.mx.Unlock()
+	b.mx.Lock()
+	b.blnc += amount
 }
--- exercise6/problem2/problem2.go
+++ exercise6/problem2/problem2.go
@@ -1,6 +1,7 @@
 package problem2
 
 import (
+	"sync"
 	"time"
 )
 
@@ -8,13 +9,30 @@ var readDelay = 10 * time.Millisecond
 
 type bankAccount struct {
 	blnc int
+	mx   *sync.RWMutex
 }
 
 func newAccount(blnc int) *bankAccount {
-	return &bankAccount{blnc}
+	return &bankAccount{blnc, &sync.RWMutex{}}
 }
 
 func (b *bankAccount) balance() int {
 	time.Sleep(readDelay)
-	return 0
+	defer b.mx.RUnlock()
+	b.mx.RLock()
+	return b.blnc
+}
+
+func (b *bankAccount) withdraw(amount int) {
+	defer b.mx.Unlock()
+	b.mx.Lock()
+	if b.blnc >= amount {
+		b.blnc -= amount
+	}
+}
+
+func (b *bankAccount) deposit(amount int) {
+	defer b.mx.Unlock()
+	b.mx.Lock()
+	b.blnc += amount
 }
