63db03586d5fa50470e9087b0a1f49d2555df7a2
eraga <eraga0423@gmail.com>
2024-11-25 13:08:00 +0500

    fix:problem: 1,2,3,4

--- exercise6/problem1/problem1.go
+++ exercise6/problem1/problem1.go
@@ -1,9 +1,30 @@
 package problem1
 
+import "sync"
+
 type bankAccount struct {
 	blnc int
+	mu   sync.Mutex
 }
 
 func newAccount(blnc int) *bankAccount {
-	return &bankAccount{blnc}
+
+	return &bankAccount{blnc: blnc}
+}
+
+func (b *bankAccount) deposit(plus int) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	b.blnc += plus
+
+}
+func (b *bankAccount) withdraw(minus int) bool {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	if b.blnc >= minus {
+		b.blnc -= minus
+		return true
+	}
+	return true
+
 }
--- exercise6/problem5/problem5.go
+++ exercise6/problem5/problem5.go
@@ -1,31 +1,53 @@
+// problem5.go
 package problem5
 
 import (
+	"sync"
 	"time"
 )
 
-func worker(id int, shoppingList *[]string, ch chan<- int) {
-	// TODO wait for shopping list to be completed
+func worker(id int, shoppingList *[]string, ch chan<- int, wg *sync.WaitGroup, mu *sync.Mutex, isReady *bool, cond *sync.Cond) {
+	defer wg.Done()
+
+	mu.Lock()
+	for !*isReady {
+		cond.Wait()
+	}
+	mu.Unlock()
+
+	// Send the worker ID after releasing the lock
 	ch <- id
 }
 
-func updateShopList(shoppingList *[]string) {
+func updateShopList(shoppingList *[]string, mu *sync.Mutex, isReady *bool, cond *sync.Cond) {
 	time.Sleep(10 * time.Millisecond)
 
-	*shoppingList = append(*shoppingList, "apples")
-	*shoppingList = append(*shoppingList, "milk")
-	*shoppingList = append(*shoppingList, "bake soda")
+	mu.Lock()
+	*shoppingList = append(*shoppingList, "apples", "milk", "bake soda")
+	*isReady = true
+	cond.Broadcast()
+	mu.Unlock()
 }
 
 func notifyOnShopListUpdate(shoppingList *[]string, numWorkers int) <-chan int {
-	notifier := make(chan int)
+	notifier := make(chan int, numWorkers)
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	isReady := false
+	cond := sync.NewCond(&mu)
 
-	for i := range numWorkers {
-		go worker(i+1, shoppingList, notifier)
-		time.Sleep(time.Millisecond) // order matters
+	for i := 0; i < numWorkers; i++ {
+		wg.Add(1)
+		go worker(i+1, shoppingList, notifier, &wg, &mu, &isReady, cond)
+		time.Sleep(time.Millisecond) // Order matters
 	}
 
-	go updateShopList(shoppingList)
+	go updateShopList(shoppingList, &mu, &isReady, cond)
+
+	go func() {
+		wg.Wait()
+		close(notifier)
+	}()
 
 	return notifier
 }
