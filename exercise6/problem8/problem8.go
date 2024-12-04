17e3cd2e86720646d6f9af42d083d8a9ec2d9b5e
Aray Mankenov <ara.nanan@yandex.ru>
2024-12-04 22:57:45 +0500

    solved ex6

--- exercise6/problem8/problem8.go
+++ exercise6/problem8/problem8.go
@@ -1,3 +1,30 @@
 package problem8
 
-func multiplex(chs []<-chan string) []string {}
+func multiplex(chs []<-chan string) []string {
+	var result []string
+	done := make(chan struct{})
+	resultChan := make(chan string)
+	remaining := len(chs)
+
+	for _, ch := range chs {
+		go func(c <-chan string) {
+			for val := range c {
+				resultChan <- val
+			}
+			remaining--
+			if remaining == 0 {
+				close(resultChan)
+			}
+		}(ch)
+	}
+
+	go func() {
+		for val := range resultChan {
+			result = append(result, val)
+		}
+		close(done)
+	}()
+
+	<-done
+	return result
+}
