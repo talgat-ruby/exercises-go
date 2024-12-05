bd6c3f9f5ec1887991f8c8bf198be19cd3a630ac
Madinab99999 <bimakanova@mail.ru>
2024-12-05 21:32:23 +0600

    Update function UpdateTags() in exercise 7

--- exercise7/blogging-platform/internal/db/post/update_post.go
+++ exercise7/blogging-platform/internal/db/post/update_post.go
@@ -78,8 +78,47 @@ func (p *Post) UpdateInfoPost(ctx context.Context, tx *sql.Tx, id_post int, id_c
 }
 
 func (p *Post) UpdateTags(ctx context.Context, tx *sql.Tx, id_post int, tags []string) ([]*blog.Tag, error) {
-	log := p.logger.With("method", "UpdatePost")
 
+	// Шаг 1: Получение текущих тегов для поста
+	currentTags, err := p.getCurrentTags(ctx, tx, id_post)
+	if err != nil {
+		return nil, err
+	}
+
+	// Шаг 2: Обработка новых тегов
+	newTagIDs, err := p.processNewTags(ctx, tx, tags)
+	if err != nil {
+		return nil, err
+	}
+
+	// Шаг 3: Удаление старых тегов, которые больше не привязаны к посту
+	err = p.deleteOldTags(ctx, tx, id_post, currentTags, newTagIDs)
+	if err != nil {
+		return nil, err
+	}
+
+	// Шаг 4: Добавление новых связей тегов с постом
+	err = p.addNewTags(ctx, tx, id_post, newTagIDs)
+	if err != nil {
+		return nil, err
+	}
+
+	// Шаг 5: Получение обновленных теги для поста
+	updatedTags, err := p.getCurrentTags(ctx, tx, id_post)
+	if err != nil {
+		return nil, err
+	}
+
+	var tagsSlice []*blog.Tag
+	for _, tag := range updatedTags {
+		tagsSlice = append(tagsSlice, tag)
+	}
+
+	return tagsSlice, nil
+}
+
+func (p *Post) getCurrentTags(ctx context.Context, tx *sql.Tx, id_post int) (map[int]*blog.Tag, error) {
+	log := p.logger.With("method", "UpdatePost")
 	query := `
 	SELECT t.id, t.name
 	FROM tag t
@@ -97,16 +136,20 @@ func (p *Post) UpdateTags(ctx context.Context, tx *sql.Tx, id_post int, tags []s
 	for rows.Next() {
 		var tag blog.Tag
 		if err := rows.Scan(&tag.ID, &tag.Name); err != nil {
-			log.ErrorContext(ctx, "fail to scanning tag", "error", err)
+			log.ErrorContext(ctx, "fail to scan tag", "error", err)
 			return nil, err
 		}
 		currentTags[tag.ID] = &tag
 	}
 
+	return currentTags, nil
+}
+
+func (p *Post) processNewTags(ctx context.Context, tx *sql.Tx, tags []string) ([]int, error) {
+	log := p.logger.With("method", "UpdatePost")
 	var newTagIDs []int
 	for _, tagName := range tags {
 		var tag blog.Tag
-
 		err := tx.QueryRowContext(ctx, "SELECT id, name FROM tag WHERE name = $1", tagName).Scan(&tag.ID, &tag.Name)
 		if err != nil && err != sql.ErrNoRows {
 			log.ErrorContext(ctx, "fail to query tag", "error", err)
@@ -114,68 +157,49 @@ func (p *Post) UpdateTags(ctx context.Context, tx *sql.Tx, id_post int, tags []s
 		}
 
 		if err == sql.ErrNoRows {
-
 			err := tx.QueryRowContext(ctx, "INSERT INTO tag(name) VALUES($1) RETURNING id", tagName).Scan(&tag.ID)
 			if err != nil {
-				log.ErrorContext(ctx, "fail to insert new tag", "error", err)
+				p.logger.ErrorContext(ctx, "fail to insert new tag", "error", err)
 				return nil, err
 			}
 		}
-
 		newTagIDs = append(newTagIDs, tag.ID)
 	}
+	return newTagIDs, nil
+}
 
-	for tagID, _ := range currentTags {
+func (p *Post) deleteOldTags(ctx context.Context, tx *sql.Tx, id_post int, currentTags map[int]*blog.Tag, newTagIDs []int) error {
+	log := p.logger.With("method", "UpdatePost")
+	for tagID := range currentTags {
 		if !contains(newTagIDs, tagID) {
 			_, err := tx.ExecContext(ctx, "DELETE FROM post_tags WHERE id_post = $1 AND id_tag = $2", id_post, tagID)
 			if err != nil {
 				log.ErrorContext(ctx, fmt.Sprintf("fail to delete tag %d from post %d", tagID, id_post), "error", err)
-				return nil, err
+				return err
 			}
 		}
 	}
+	return nil
+}
 
+func (p *Post) addNewTags(ctx context.Context, tx *sql.Tx, id_post int, newTagIDs []int) error {
+	log := p.logger.With("method", "UpdatePost")
 	for _, tagID := range newTagIDs {
-
 		var count int
 		err := tx.QueryRowContext(ctx, "SELECT COUNT(*) FROM post_tags WHERE id_post = $1 AND id_tag = $2", id_post, tagID).Scan(&count)
 		if err != nil {
-			log.ErrorContext(ctx, fmt.Sprintf("fail to checking if post is already linked with tag: %d", tagID), "error", err)
-			return nil, err
+			log.ErrorContext(ctx, fmt.Sprintf("fail to check if post is already linked with tag: %d", tagID), "error", err)
+			return err
 		}
 		if count == 0 {
 			_, err := tx.ExecContext(ctx, "INSERT INTO post_tags(id_post, id_tag) VALUES($1, $2)", id_post, tagID)
 			if err != nil {
 				log.ErrorContext(ctx, fmt.Sprintf("fail to insert post_tag for post %d and tag %d", id_post, tagID), "error", err)
-				return nil, err
+				return err
 			}
 		}
 	}
-
-	query = `
-	SELECT t.id, t.name
-	FROM tag t
-	JOIN post_tags pt ON pt.id_tag = t.id
-	WHERE pt.id_post = $1
-	`
-	rows, err = tx.QueryContext(ctx, query, id_post)
-	if err != nil {
-		log.ErrorContext(ctx, fmt.Sprintf("fail getting updated tags for post %d", id_post), "error", err)
-		return nil, err
-	}
-	defer rows.Close()
-
-	var updatedTags []*blog.Tag
-	for rows.Next() {
-		var tag blog.Tag
-		if err := rows.Scan(&tag.ID, &tag.Name); err != nil {
-			log.ErrorContext(ctx, "fail scanning updated tag", "error", err)
-			return nil, err
-		}
-		updatedTags = append(updatedTags, &tag)
-	}
-
-	return updatedTags, nil
+	return nil
 }
 
 func contains(slice []int, val int) bool {
