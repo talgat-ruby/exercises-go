91f1c13a91149ed3fff6e35497f6fd31530a05be
tobirama <g.kemalatdin@documentolog.com>
2024-12-10 18:49:07 +0500

    fix: refactoring

--- exercise7/blogging-platform/main.go
+++ exercise7/blogging-platform/main.go
@@ -3,16 +3,18 @@ package main
 import (
 	"context"
 	"github.com/talgat-ruby/exercises-go/exercise7/blogging-platform/internal/api"
-	config2 "github.com/talgat-ruby/exercises-go/exercise7/blogging-platform/internal/config"
+	conf "github.com/talgat-ruby/exercises-go/exercise7/blogging-platform/internal/config"
 	db "github.com/talgat-ruby/exercises-go/exercise7/blogging-platform/internal/db"
 	"log/slog"
+	"os"
+	"os/signal"
 )
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 
 	//config
-	config, err := config2.NewConfig()
+	_, err := conf.NewConfig()
 	if err != nil {
 		slog.ErrorContext(
 			ctx,
@@ -35,8 +37,25 @@ func main() {
 	}
 
 	// api
-	a := api.NewApi(config, dbConnect)
+	a := api.NewApi(slog.With("service", "api"), dbConnect)
 	slog.InfoContext(ctx, "initialize service", "service", "api")
-	a.Start(ctx, cancel)
+
+	go func(ctx context.Context, cancelFunc context.CancelFunc) {
+		if err := a.Start(ctx); err != nil {
+			slog.ErrorContext(ctx, "failed to start api", "error", err.Error())
+		}
+		cancelFunc()
+	}(ctx, cancel)
+
+	go func(cancelFunc context.CancelFunc) {
+		shutdown := make(chan os.Signal, 1)   // Create channel to signify s signal being sent
+		signal.Notify(shutdown, os.Interrupt) // When an interrupt is sent, notify the channel
+
+		sig := <-shutdown
+		slog.WarnContext(ctx, "signal received - shutting down...", "signal", sig)
+
+		cancelFunc()
+	}(cancel)
+
 	<-ctx.Done()
 }
