46e74117cafbeb57e8ffb2044eebfe8fe435db08
eraga <eraga0423@gmail.com>
2025-01-05 16:33:03 +0500

    fix :error start program, migrations, postgres, compose

--- exercise8/expense_tracker/cmd/main.go
+++ exercise8/expense_tracker/cmd/main.go
@@ -1,10 +1,24 @@
 package main
 
-import "tracker/internal/api"
+import (
+	"log"
+	"log/slog"
+	"os"
+
+	"tracker/internal/api"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	err := api.StartServer()
+	err := godotenv.Load("../.env")
 	if err != nil {
+		log.Println("gogdotenverror:", err)
+	}
+	log.Println(os.Getenv("API_PORT"))
+	err = api.StartServer()
+	if err != nil {
+		slog.Error("error start program")
 		return
 	}
 }
