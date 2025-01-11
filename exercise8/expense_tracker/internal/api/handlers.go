539c2924d083c69dbd7a1bd7feb19759ec400d90
eraga <eraga0423@gmail.com>
2025-01-11 16:52:54 +0500

    finish

--- exercise8/expense_tracker/internal/api/handlers.go
+++ exercise8/expense_tracker/internal/api/handlers.go
@@ -13,12 +13,6 @@ import (
 	"tracker/internal/service"
 )
 
-// type Router struct {
-// 	router  *http.ServeMux
-
-// 	midd    *middleware.Middleware
-// }
-
 func BasicHandlers(mux *http.ServeMux, newdb db.ExpencesDBSt) {
 	serviceExpence := service.NewServiceExpence(newdb)
 	handlerExpence := handler.NewHandlerExpence(serviceExpence)
